a21d80de31c2b453d0b4e3fec0294cb780f0499a
vanbreda@btinternet.com <vanbreda@btinternet.com@642a62a4-ac1b-11dd-9a07-0f3041a8e97c>
2010-09-09 14:09:38 +0000

    MNHNL_BTW rewrite, to use AJAX forms to submit, and WMS layer for the main survey selection view. Other minor tweaks as weel to termlist ordering, configurable navigation, species filtering. Need to confirm about validation translation.

---
+++ support_files/MNHNL/WMS_View.sql
@@ -0,0 +1,56 @@
+create view btw_transects as select name, id, boundary_geom from locations where parent_id is null;
+
+//In geoserver, Create a style as defined below.
+//Create layers for the above view, and for the locations table, which both use the style.
+
+// map now builds a WMS overlay.
+// WMS overlay can reference the btw_transects as above.
+// Now transects layer style set to display the lines as thicker and only display names if not too far zoomed out.
+// Sect Map location_layer dettails in iform param to => Name:Location,URL:http://localhost/geoserver/wms,LAYERS:indicia:btw_transects,SRS:EPSG:2169,FORMAT:image/png,minScale:0,maxScale:10000,units:m
+
+<?xml version="1.0" encoding="ISO-8859-1"?>
+<StyledLayerDescriptor version="1.0.0" xmlns="http://www.opengis.net/sld" xmlns:ogc="http://www.opengis.net/ogc"
+  xmlns:xlink="http://www.w3.org/1999/xlink" xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance"
+  xsi:schemaLocation="http://www.opengis.net/sld http://schemas.opengis.net/sld/1.0.0/StyledLayerDescriptor.xsd">
+  <NamedLayer>
+    <Name>redflag</Name>    
+    <UserStyle>
+      <Title>1 px blue line</Title>
+      <IsDefault>1</IsDefault>
+      <FeatureTypeStyle>
+        <!--FeatureTypeName>Feature</FeatureTypeName-->
+        <Rule>
+          <Title>Blue Line</Title>
+          <LineSymbolizer>
+            <Geometry>
+              <ogc:PropertyName>boundary_geom</ogc:PropertyName>
+            </Geometry>
+            <Stroke>
+              <CssParameter name="stroke">#0000FF</CssParameter>
+              <CssParameter name="stroke-width">2</CssParameter>
+              <CssParameter name="stroke-linecap">round</CssParameter>
+            </Stroke>
+          </LineSymbolizer>
+        </Rule>
+        <Rule>
+          <MaxScaleDenominator>1000000</MaxScaleDenominator>
+          <TextSymbolizer>
+            <Label>
+              <ogc:PropertyName>name</ogc:PropertyName>
+            </Label>
+            <Font>
+              <CssParameter name="font-family">Arial</CssParameter>
+              <CssParameter name="font-family">Sans-Serif</CssParameter>
+              <CssParameter name="font-style">bold</CssParameter>
+              <CssParameter name="font-size">12</CssParameter>
+              <CssParameter name="font-color">#FF0000</CssParameter>
+            </Font>
+            <Halo></Halo>
+          </TextSymbolizer>
+        </Rule>
+      </FeatureTypeStyle>
+    </UserStyle>
+  </NamedLayer>
+</StyledLayerDescriptor> 
+
+
