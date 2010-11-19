067a8c90b7764184d2a07d43da567d90747bc2cd
vanbreda@btinternet.com <vanbreda@btinternet.com@642a62a4-ac1b-11dd-9a07-0f3041a8e97c>
2010-11-19 00:40:22 +0000

    MNHNL Butterflies: remove now deletes the removed occurrences, and Adds new attributes to botom of section list.

--- support_files/MNHNL/buterfly_attributes.sql
+++ support_files/MNHNL/buterfly_attributes.sql
@@ -14,4 +14,27 @@ INSERT INTO occurrence_attributes (
 	'Butterfly Qual Dist', 'L', now(), 1, now(), 1, (select id from termlists where external_key='butterfly:distribution'), 'f', 't'
 );
 
+--- use standard count, though make sure not required.
 
+--- section sample attributes
+INSERT INTO termlists (title, description, created_on, created_by_id, updated_on, updated_by_id, external_key)
+VALUES ('HabitatType', 'Habitat Type for this Section.', now(), 1, now(), 1, 'butterfly:habitat');
+SELECT insert_term('URB Milieux urbanisés', 'fra', null, 'butterfly:habitat');
+SELECT insert_term('AGR Cultures annuelles', 'fra', null, 'butterfly:habitat');
+SELECT insert_term('PRA Prairies et herbages', 'fra', null, 'butterfly:habitat');
+SELECT insert_term('FFE Forêts de feuillus', 'fra', null, 'butterfly:habitat');
+SELECT insert_term('PEL Pelouses', 'fra', null, 'butterfly:habitat');
+SELECT insert_term('FRI Friches', 'fra', null, 'butterfly:habitat');
+SELECT insert_term('BUI Milieux buissonneux', 'fra', null, 'butterfly:habitat');
+SELECT insert_term('ZHU Zones humides', 'fra', null, 'butterfly:habitat');
+INSERT INTO sample_attributes (
+	caption, data_type, created_on, created_by_id, updated_on, updated_by_id, termlist_id, multi_value, public
+) VALUES (
+	'Habitat Type', 'L', now(), 1, now(), 1, (select id from termlists where external_key='butterfly:habitat'), 'f', 't'
+);
+
+INSERT INTO sample_attributes (
+	caption, data_type, created_on, created_by_id, updated_on, updated_by_id, multi_value, public
+) VALUES (
+	'Aucune Observation', 'B', now(), 1, now(), 1, 'f', 't'
+);
