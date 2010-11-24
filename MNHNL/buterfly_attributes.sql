1e5d9bdc20c653c3d8b2d825d9d8695893b3c21e
vanbreda <vanbreda@btinternet.com>
2010-11-24 13:32:46 +0000

    Next drop for MNHNL Butterflies following comments from Nicolas. Dynamic 1 extended to allow iform_location_tools allocation. Still have issues with Validation.

--- MNHNL/buterfly_attributes.sql
+++ MNHNL/buterfly_attributes.sql
@@ -19,14 +19,14 @@ INSERT INTO occurrence_attributes (
 --- section sample attributes
 INSERT INTO termlists (title, description, created_on, created_by_id, updated_on, updated_by_id, external_key)
 VALUES ('HabitatType', 'Habitat Type for this Section.', now(), 1, now(), 1, 'butterfly:habitat');
-SELECT insert_term('URB Milieux urbanisés', 'fra', null, 'butterfly:habitat');
-SELECT insert_term('AGR Cultures annuelles', 'fra', null, 'butterfly:habitat');
-SELECT insert_term('PRA Prairies et herbages', 'fra', null, 'butterfly:habitat');
-SELECT insert_term('FFE Forêts de feuillus', 'fra', null, 'butterfly:habitat');
-SELECT insert_term('PEL Pelouses', 'fra', null, 'butterfly:habitat');
-SELECT insert_term('FRI Friches', 'fra', null, 'butterfly:habitat');
-SELECT insert_term('BUI Milieux buissonneux', 'fra', null, 'butterfly:habitat');
-SELECT insert_term('ZHU Zones humides', 'fra', null, 'butterfly:habitat');
+SELECT insert_term('URB', 'fra', null, 'butterfly:habitat'); ---  Milieux urbanisés
+SELECT insert_term('AGR', 'fra', null, 'butterfly:habitat'); --- Cultures annuelles
+SELECT insert_term('PRA', 'fra', null, 'butterfly:habitat'); --- Prairies et herbages
+SELECT insert_term('FFE', 'fra', null, 'butterfly:habitat'); --- Forêts de feuillus
+SELECT insert_term('PEL', 'fra', null, 'butterfly:habitat'); --- Pelouses
+SELECT insert_term('FRI', 'fra', null, 'butterfly:habitat'); --- Friches
+SELECT insert_term('BUI', 'fra', null, 'butterfly:habitat'); --- Milieux buissonneux
+SELECT insert_term('ZHU', 'fra', null, 'butterfly:habitat'); --- Zones humides
 INSERT INTO sample_attributes (
 	caption, data_type, created_on, created_by_id, updated_on, updated_by_id, termlist_id, multi_value, public
 ) VALUES (
@@ -38,3 +38,15 @@ INSERT INTO sample_attributes (
 ) VALUES (
 	'Aucune Observation', 'B', now(), 1, now(), 1, 'f', 't'
 );
+
+INSERT INTO termlists (title, description, created_on, created_by_id, updated_on, updated_by_id, external_key)
+VALUES ('Reliability', 'Reliability of data for this Section.', now(), 1, now(), 1, 'butterfly:reliability');
+SELECT insert_term('1', 'fra', null, 'butterfly:reliability'); --- Reliable data
+SELECT insert_term('2', 'fra', null, 'butterfly:reliability'); --- Weakly reliable data
+SELECT insert_term('3', 'fra', null, 'butterfly:reliability'); --- Unreliable data
+INSERT INTO sample_attributes (
+	caption, data_type, created_on, created_by_id, updated_on, updated_by_id, termlist_id, multi_value, public
+) VALUES (
+	'Fiabilité du comptage', 'L', now(), 1, now(), 1, (select id from termlists where external_key='butterfly:reliability'), 'f', 't'
+);
+
