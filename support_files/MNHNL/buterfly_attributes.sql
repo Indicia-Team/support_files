d35b621e76fcf27dc33327c3e40764348893e5d2
vanbreda@btinternet.com <vanbreda@btinternet.com@642a62a4-ac1b-11dd-9a07-0f3041a8e97c>
2011-03-03 00:05:40 +0000

    First drop of MNHNL Bats form

--- support_files/MNHNL/buterfly_attributes.sql
+++ support_files/MNHNL/buterfly_attributes.sql
@@ -127,7 +127,7 @@ SELECT insert_term('90%', 'fra', null, 'butterfly:cloud');
 SELECT insert_term('95%', 'fra', null, 'butterfly:cloud');
 SELECT insert_term('100%', 'fra', null, 'butterfly:cloud');
 INSERT INTO sample_attributes (
-	caption, data_type, created_on, created_by_id, updated_on, updated_by_id, multi_value, public
+	caption, data_type, created_on, created_by_id, updated_on, updated_by_id, termlist_id, multi_value, public
 ) VALUES (
 	'Cloud Cover', 'L', now(), 1, now(), 1, (select id from termlists where external_key='butterfly:cloud'), 'f', 't'
 );
