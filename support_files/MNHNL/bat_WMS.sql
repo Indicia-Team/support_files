4b09c2c991e542ce6fdcb9e937670e6ef06367f5
vanbreda@btinternet.com <vanbreda@btinternet.com@642a62a4-ac1b-11dd-9a07-0f3041a8e97c>
2011-04-01 09:57:52 +0000

    Intermediate update to MNHNL Winter Bats to address points brought up by Nicolas T.

---
+++ support_files/MNHNL/bat_WMS.sql
@@ -0,0 +1,98 @@
+DROP VIEW bat_locations; 
+CREATE VIEW bat_locations  AS 
+ SELECT l.id, l.name, l.code, l.centroid_geom as the_geom, lw.website_id
+   FROM locations l
+   LEFT JOIN locations_websites lw ON l.id = lw.location_id and lw.website_id=3
+   WHERE l.parent_id IS NULL AND l.deleted = FALSE AND lw.deleted = FALSE;
+
+<FeatureTypeStyle>
+     <Rule>
+       <PointSymbolizer>
+         <Graphic>
+           <Mark>
+             <WellKnownName>circle</WellKnownName>
+             <Fill>
+               <CssParameter name="fill">#FF0000</CssParameter>
+             </Fill>
+           </Mark>
+           <Size>6</Size>
+         </Graphic>
+       </PointSymbolizer>
+       <TextSymbolizer>
+         <Label>
+           <ogc:PropertyName>name</ogc:PropertyName>
+         </Label>
+         <Font>
+           <CssParameter name="font-family">Arial</CssParameter>
+           <CssParameter name="font-size">12</CssParameter>
+           <CssParameter name="font-style">normal</CssParameter>
+           <CssParameter name="font-weight">bold</CssParameter>
+         </Font>
+         <LabelPlacement>
+           <PointPlacement>
+             <AnchorPoint>
+               <AnchorPointX>0.5</AnchorPointX>
+               <AnchorPointY>0.0</AnchorPointY>
+             </AnchorPoint>
+             <Displacement>
+               <DisplacementX>0</DisplacementX>
+               <DisplacementY>5</DisplacementY>
+             </Displacement>
+           </PointPlacement>
+         </LabelPlacement>
+         <Fill>
+           <CssParameter name="fill">#000000</CssParameter>
+         </Fill>
+       </TextSymbolizer>
+     </Rule>
+   </FeatureTypeStyle>
+ 
+
+<?xml version="1.0" encoding="ISO-8859-1"?>
+<StyledLayerDescriptor version="1.0.0" xmlns="http://www.opengis.net/sld" xmlns:ogc="http://www.opengis.net/ogc"
+  xmlns:xlink="http://www.w3.org/1999/xlink" xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance"
+  xsi:schemaLocation="http://www.opengis.net/sld http://schemas.opengis.net/sld/1.0.0/StyledLayerDescriptor.xsd">
+  <NamedLayer>
+    <Name>batloc</Name>    
+    <UserStyle>
+      <Title>Point and Name</Title>
+      <Abstract>A style that prints out a 6px wide red square with a name</Abstract>
+
+      <FeatureTypeStyle>
+        <!--FeatureTypeName>Feature</FeatureTypeName-->
+        <Rule>
+          <Title>Red square</Title>
+          <PointSymbolizer>
+            <Graphic>
+              <Mark>
+                <WellKnownName>square</WellKnownName>
+                <Fill>
+                  <CssParameter name="fill">#FF0000</CssParameter>
+                </Fill>
+              </Mark>
+              <Size>6</Size>
+            </Graphic>
+          </PointSymbolizer>
+        </Rule>
+        <Rule>
+          <MaxScaleDenominator>1000000</MaxScaleDenominator>
+          <TextSymbolizer>
+            <Label>
+              <ogc:PropertyName>name</ogc:PropertyName>
+            </Label>
+            <Font>
+              <CssParameter name="font-family">Arial</CssParameter>
+              <CssParameter name="font-family">Sans-Serif</CssParameter>
+              <CssParameter name="font-style">bold</CssParameter>
+              <CssParameter name="font-size">12</CssParameter>
+              <CssParameter name="font-color">#FF0000</CssParameter>
+            </Font>
+            <Halo></Halo>
+          </TextSymbolizer>
+        </Rule>
+      </FeatureTypeStyle>
+    </UserStyle>
+  </NamedLayer>
+</StyledLayerDescriptor> 
+
+
