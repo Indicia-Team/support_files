f82ae8b061b79833f8845dbe7f24d0abdd1dc270
vanbreda <vanbreda@btinternet.com>
2012-02-23 10:47:05 +0000

    MNHNL SHAPE file download,

--- MNHNL/Location_SHP_Views.sql
+++ MNHNL/Location_SHP_Views.sql
@@ -1,41 +1,75 @@
+--- where there is no boundary geometry, assume point centroid
 DROP VIEW POINT_LOCATIONS;
-CREATE OR REPLACE VIEW POINT_LOCATIONS (id, location_type_id, name, code, geom) AS
-select id, location_type_id, name, code, ST_CollectionExtract(boundary_geom, 1) from locations
-WHERE GeometryType(boundary_geom) = 'GEOMETRYCOLLECTION'
-AND NOT ST_IsEmpty(ST_CollectionExtract(boundary_geom, 1))
+CREATE OR REPLACE VIEW POINT_LOCATIONS (geom, code, name, location_type, id, location_type_id, website_id) AS
+select ST_CollectionExtract(l.boundary_geom, 1), l.code, l.name, ttl.term, l.id, l.location_type_id, lw.website_id from locations l
+ JOIN locations_websites lw ON (l.id = lw.location_id and lw.deleted = false)
+ JOIN detail_termlists_terms ttl on (l.location_type_id = ttl.id and ttl.preferred=true)
+ WHERE l.deleted = false
+ AND GeometryType(boundary_geom) = 'GEOMETRYCOLLECTION'
+ AND NOT ST_IsEmpty(ST_CollectionExtract(boundary_geom, 1))
 UNION
-select id, location_type_id, name, code, boundary_geom from locations
-WHERE GeometryType(boundary_geom) = 'MULTIPOINT'
+select l.boundary_geom, l.code, l.name, ttl.term, l.id, l.location_type_id, lw.website_id from locations l
+ JOIN locations_websites lw ON (l.id = lw.location_id and lw.deleted = false)
+ JOIN detail_termlists_terms ttl on (l.location_type_id = ttl.id and ttl.preferred=true)
+ WHERE l.deleted = false
+ AND GeometryType(boundary_geom) = 'MULTIPOINT'
 UNION
-select id, location_type_id, name, code, ST_MULTI(boundary_geom) from locations
-WHERE GeometryType(boundary_geom) = 'POINT'
-ORDER by id
+select ST_MULTI(boundary_geom), l.code, l.name, ttl.term, l.id, l.location_type_id, lw.website_id from locations l
+ JOIN locations_websites lw ON (l.id = lw.location_id and lw.deleted = false)
+ JOIN detail_termlists_terms ttl on (l.location_type_id = ttl.id and ttl.preferred=true)
+ WHERE l.deleted = false
+ AND GeometryType(boundary_geom) = 'POINT'
+UNION
+select ST_MULTI(centroid_geom), l.code, l.name, ttl.term, l.id, l.location_type_id, lw.website_id from locations l
+ JOIN locations_websites lw ON (l.id = lw.location_id and lw.deleted = false)
+ JOIN detail_termlists_terms ttl on (l.location_type_id = ttl.id and ttl.preferred=true)
+ WHERE l.deleted = false
+ AND GeometryType(centroid_geom) = 'POINT'
+ORDER by name,id
 ;
 
 DROP VIEW LINE_LOCATIONS;
-CREATE OR REPLACE VIEW LINE_LOCATIONS (id, location_type_id, name, code, geom) AS
-select id, location_type_id, name, code, ST_CollectionExtract(boundary_geom, 2) from locations
-WHERE GeometryType(boundary_geom) = 'GEOMETRYCOLLECTION'
-AND NOT ST_IsEmpty(ST_CollectionExtract(boundary_geom, 2))
+CREATE OR REPLACE VIEW LINE_LOCATIONS (geom, code, name, location_type, id, location_type_id, website_id) AS
+select ST_CollectionExtract(l.boundary_geom, 2), l.code, l.name, ttl.term, l.id, l.location_type_id, lw.website_id from locations l
+ JOIN locations_websites lw ON (l.id = lw.location_id and lw.deleted = false)
+ JOIN detail_termlists_terms ttl on (l.location_type_id = ttl.id and ttl.preferred=true)
+ WHERE l.deleted = false
+ AND GeometryType(boundary_geom) = 'GEOMETRYCOLLECTION'
+ AND NOT ST_IsEmpty(ST_CollectionExtract(boundary_geom, 2))
 UNION
-select id, location_type_id, name, code, boundary_geom from locations
-WHERE GeometryType(boundary_geom) = 'MULTILINE'
+select l.boundary_geom, l.code, l.name, ttl.term, l.id, l.location_type_id, lw.website_id from locations l
+ JOIN locations_websites lw ON (l.id = lw.location_id and lw.deleted = false)
+ JOIN detail_termlists_terms ttl on (l.location_type_id = ttl.id and ttl.preferred=true)
+ WHERE l.deleted = false
+ AND GeometryType(boundary_geom) = 'MULTILINE'
 UNION
-select id, location_type_id, name, code, ST_MULTI(boundary_geom) from locations
-WHERE GeometryType(boundary_geom) = 'LINE'
-ORDER by id
+select ST_MULTI(boundary_geom), l.code, l.name, ttl.term, l.id, l.location_type_id, lw.website_id from locations l
+ JOIN locations_websites lw ON (l.id = lw.location_id and lw.deleted = false)
+ JOIN detail_termlists_terms ttl on (l.location_type_id = ttl.id and ttl.preferred=true)
+ WHERE l.deleted = false
+ AND GeometryType(boundary_geom) = 'LINE'
+ORDER by name,id
 ;
 
 DROP VIEW POLYGON_LOCATIONS;
-CREATE OR REPLACE VIEW POLYGON_LOCATIONS (id, location_type_id, name, code, geom) AS
-select id, location_type_id, name, code, ST_CollectionExtract(boundary_geom, 3) from locations
-WHERE GeometryType(boundary_geom) = 'GEOMETRYCOLLECTION'
-AND NOT ST_IsEmpty(ST_CollectionExtract(boundary_geom, 3))
+CREATE OR REPLACE VIEW POLYGON_LOCATIONS (geom, code, name, location_type, id, location_type_id, website_id) AS
+select ST_CollectionExtract(l.boundary_geom, 1), l.code, l.name, ttl.term, l.id, l.location_type_id, lw.website_id from locations l
+ JOIN locations_websites lw ON (l.id = lw.location_id and lw.deleted = false)
+ JOIN detail_termlists_terms ttl on (l.location_type_id = ttl.id and ttl.preferred=true)
+ WHERE l.deleted = false
+ AND GeometryType(boundary_geom) = 'GEOMETRYCOLLECTION'
+ AND NOT ST_IsEmpty(ST_CollectionExtract(boundary_geom, 1))
 UNION
-select id, location_type_id, name, code, boundary_geom from locations
-WHERE GeometryType(boundary_geom) = 'MULTIPOLYGON'
+select l.boundary_geom, l.code, l.name, ttl.term, l.id, l.location_type_id, lw.website_id from locations l
+ JOIN locations_websites lw ON (l.id = lw.location_id and lw.deleted = false)
+ JOIN detail_termlists_terms ttl on (l.location_type_id = ttl.id and ttl.preferred=true)
+ WHERE l.deleted = false
+ AND GeometryType(boundary_geom) = 'MULTIPOLYGON'
 UNION
-select id, location_type_id, name, code, boundary_geom from locations
-WHERE GeometryType(boundary_geom) = 'POLYGON'
-ORDER by id
+select ST_MULTI(boundary_geom), l.code, l.name, ttl.term, l.id, l.location_type_id, lw.website_id from locations l
+ JOIN locations_websites lw ON (l.id = lw.location_id and lw.deleted = false)
+ JOIN detail_termlists_terms ttl on (l.location_type_id = ttl.id and ttl.preferred=true)
+ WHERE l.deleted = false
+ AND GeometryType(boundary_geom) = 'POLYGON'
+ORDER by name,id
 ;
