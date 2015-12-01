679429da62b9ec4b97cbe6921232168f6d7fcf71
Andrew van Breda <andrew_van_breda@hotmail.com>
2015-12-01 08:12:50 +0000

    Last 2 quality indices added to importer

--- Pantheon Data Importer for v3.3/script 9 - quality indices.sql
+++ Pantheon Data Importer for v3.3/script 9 - quality indices.sql
@@ -483,6 +483,82 @@ END LOOP;
 END
 $do$;
 
+--Note there are 2 exposed riverine sediments fidelity score items with same name, so we use source as well for identification
+set search_path TO indicia, public;
+DO
+$do$
+declare trait_to_import RECORD;
+BEGIN 
+FOR trait_to_import IN 
+(select ittl.id as taxa_taxon_list_id,cast(pst.trait_value as integer) as insertion_val,1,now(),1,now(),ittSource.id as source
+from pantheon.tbl_species_traits pst
+join pantheon.tbl_species ps on ps.species_id=pst.species_id
+join indicia.taxa it on it.external_key=ps.preferred_tvk AND it.deleted=false
+join indicia.taxa_taxon_lists ittl on ittl.taxon_id=it.id AND ittl.taxon_list_id=<pantheon_taxon_list_id> AND ittl.deleted=false
+join pantheon.tbl_traits pt on pt.trait_id=pst.trait_id AND pt.trait_description='exposed riverine sediments fidelity score' AND pt.trait_source='Drake & Hewitt (2007)'
+--The way the source is written is not consistant, so we need to interpret these
+left join indicia.terms itSource on (itSource.term=pst.coding_convention OR
+(pst.coding_convention='from synanthropic (ISIS)' AND itSource.term='ISIS'))
+AND pst.coding_convention!='0' AND itSource.deleted=false
+left join indicia.termlists_terms ittSource on ittSource.term_id = itSource.id AND ittSource.deleted=false
+left join indicia.termlists itlSource on itlSource.id = ittSource.termlist_id AND itlSource.title = 'Attribute value sources' AND ittSource.deleted=false
+GROUP BY ps.preferred_tvk,ps.species_tvk,pst.trait_value,ittl.id,ittSource.id
+ORDER BY ps.species_tvk=ps.preferred_tvk desc
+) loop
+IF (NOT EXISTS (
+select ttlav2.id
+from taxa_taxon_list_attribute_values ttlav2
+join taxa_taxon_lists ttl2 on ttl2.id = ttlav2.taxa_taxon_list_id AND ttl2.id=trait_to_import.taxa_taxon_list_id  AND ttl2.deleted=false
+where ttlav2.taxa_taxon_list_attribute_id = (select id from taxa_taxon_list_attributes where caption='exposed riverine sediments fidelity score (D & H)' and deleted=false) AND ttlav2.deleted=false
+ORDER BY ttlav2.id desc
+LIMIT 1))
+THEN
+insert into
+indicia.taxa_taxon_list_attribute_values (taxa_taxon_list_id,taxa_taxon_list_attribute_id,int_value,created_by_id,created_on,updated_by_id,updated_on,source_id)
+values (trait_to_import.taxa_taxon_list_id,(select id from taxa_taxon_list_attributes where caption='exposed riverine sediments fidelity score (D & H)' and deleted=false),trait_to_import.insertion_val,1,now(),1,now(),trait_to_import.source);
+ELSE 
+END IF;
+END LOOP;
+END
+$do$;
+
+set search_path TO indicia, public;
+DO
+$do$
+declare trait_to_import RECORD;
+BEGIN 
+FOR trait_to_import IN 
+(select ittl.id as taxa_taxon_list_id,cast(pst.trait_value as integer) as insertion_val,1,now(),1,now(),ittSource.id as source
+from pantheon.tbl_species_traits pst
+join pantheon.tbl_species ps on ps.species_id=pst.species_id
+join indicia.taxa it on it.external_key=ps.preferred_tvk AND it.deleted=false
+join indicia.taxa_taxon_lists ittl on ittl.taxon_id=it.id AND ittl.taxon_list_id=<pantheon_taxon_list_id> AND ittl.deleted=false
+join pantheon.tbl_traits pt on pt.trait_id=pst.trait_id AND pt.trait_description='exposed riverine sediments fidelity score' AND pt.trait_source='Sadler & Bell (2002)'
+--The way the source is written is not consistant, so we need to interpret these
+left join indicia.terms itSource on (itSource.term=pst.coding_convention OR
+(pst.coding_convention='from synanthropic (ISIS)' AND itSource.term='ISIS'))
+AND pst.coding_convention!='0' AND itSource.deleted=false
+left join indicia.termlists_terms ittSource on ittSource.term_id = itSource.id AND ittSource.deleted=false
+left join indicia.termlists itlSource on itlSource.id = ittSource.termlist_id AND itlSource.title = 'Attribute value sources' AND ittSource.deleted=false
+GROUP BY ps.preferred_tvk,ps.species_tvk,pst.trait_value,ittl.id,ittSource.id
+ORDER BY ps.species_tvk=ps.preferred_tvk desc
+) loop
+IF (NOT EXISTS (
+select ttlav2.id
+from taxa_taxon_list_attribute_values ttlav2
+join taxa_taxon_lists ttl2 on ttl2.id = ttlav2.taxa_taxon_list_id AND ttl2.id=trait_to_import.taxa_taxon_list_id  AND ttl2.deleted=false
+where ttlav2.taxa_taxon_list_attribute_id = (select id from taxa_taxon_list_attributes where caption='exposed riverine sediments fidelity score (S & B)' and deleted=false) AND ttlav2.deleted=false
+ORDER BY ttlav2.id desc
+LIMIT 1))
+THEN
+insert into
+indicia.taxa_taxon_list_attribute_values (taxa_taxon_list_id,taxa_taxon_list_attribute_id,int_value,created_by_id,created_on,updated_by_id,updated_on,source_id)
+values (trait_to_import.taxa_taxon_list_id,(select id from taxa_taxon_list_attributes where caption='exposed riverine sediments fidelity score (S & B)' and deleted=false),trait_to_import.insertion_val,1,now(),1,now(),trait_to_import.source);
+ELSE 
+END IF;
+END LOOP;
+END
+$do$;
 
 --Next do ones that use the Capital Characters termlist
 set search_path TO indicia, public;
