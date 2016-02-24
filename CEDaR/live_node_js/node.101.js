f44d40c15d06b853936c6a7245d15360b954b330
Andrew van Breda <andrew_van_breda@hotmail.com>
2016-02-24 09:19:22 +0000

    CEDAR On True Harvest Seeds we no longer warn if no species has been entered in edit mode. This is partly because there will already be a species, so if someone has reason to deliberately remove it, then there is no real need to stop them (the original message intention was to stop people forgetting). Also as CEDaR doesn't currently have an empty clonable row in edit mode, it would complicate the code to take edit mode into account.

--- CEDaR/live_node_js/node.101.js
+++ CEDaR/live_node_js/node.101.js
@@ -3,7 +3,11 @@ jQuery(window).load(function () {
     //Check to make sure user has entered at least one species.
     //We count the visible rows in the grid to make sure there are at least 3
     //An empty grid consists of the header and an empty row.
-    if (jQuery('#[id^="species-grid"] tr:visible').length < 3) {
+    //Don't do this in edit mode, as in CEDaR there is (currently) no clonable empty row in edit mode, which makes things more complicated to code.
+    //Also we don't really need this functionality in edit mode as there will already be a species. If someone wants to remove it deliberately
+    //we may as well let them.
+    //Check occurrence_id or sample_id isn't in the URL (i.e. ignore in Edit Mode)
+    if (jQuery('#[id^="species-grid"] tr:visible').length < 3 && window.location.href.indexOf("occurrence_id") == -1 && window.location.href.indexOf("sample_id") == -1) {
       alert('Oops, you seem to have forgotten to enter the species you recorded.');
       return false;
     } else {
