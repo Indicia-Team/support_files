3b8d3fbf7b28320ddb3641c91a6e926db8744ce5
Andrew van Breda <andrew_van_breda@hotmail.com>
2016-03-23 21:55:00 +0000

    Earthworm Watch Node JS file for earthworm watch's data entry page

---
+++ Earthworm Watch/live_node_js/node.5.js
@@ -0,0 +1,31 @@
+
+jQuery(document).ready(function($) {
+  $('.scEarthwormCountCell').show();
+  $('.deh-required').hide();
+  if ($('#samples-grid').find('.empty-row').length !== 0) {
+    //If empty row exists, then it means no pit created yet, so hide the edit pit help
+    $('.edit-pit-help').hide();
+  } else {
+    $('.first-pit-help').hide();
+  }
+  default_ecosystem_type_in_add_mode();
+  
+  function default_ecosystem_type_in_add_mode() {
+    $('#sc\\:adult-soil-0\\:\\:occAttr\\:538').val(6106);
+    $('#sc\\:adult-soil-1\\:\\:occAttr\\:538').val(6107);
+    $('#sc\\:adult-soil-2\\:\\:occAttr\\:538').val(6105);
+    
+    $('#sc\\:immature-soil-0\\:\\:occAttr\\:538').val(6106);
+    $('#sc\\:immature-soil-1\\:\\:occAttr\\:538').val(6107);
+    $('#sc\\:immature-soil-2\\:\\:occAttr\\:538').val(6105);
+    
+    $('#sc\\:adult-mustard-0\\:\\:occAttr\\:538').val(6106);
+    $('#sc\\:adult-mustard-1\\:\\:occAttr\\:538').val(6107);
+    $('#sc\\:adult-mustard-2\\:\\:occAttr\\:538').val(6105);
+    
+    $('#sc\\:immature-mustard-0\\:\\:occAttr\\:538').val(6106);
+    $('#sc\\:immature-mustard-1\\:\\:occAttr\\:538').val(6107);
+    $('#sc\\:immature-mustard-2\\:\\:occAttr\\:538').val(6105);
+  }
+});
+
