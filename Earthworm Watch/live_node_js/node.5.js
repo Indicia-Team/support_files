ffc81a76fff4e0799f135aed91d69bf099c9187c
Andrew van Breda <andrew_van_breda@hotmail.com>
2016-04-21 00:19:23 +0100

    Changed code as the ordering of the grid had changed (although ideally the JS wouldn't be linked to the row numbers). This means the correct ecosystems are still associated with the earthworm types.

--- Earthworm Watch/live_node_js/node.5.js
+++ Earthworm Watch/live_node_js/node.5.js
@@ -9,23 +9,23 @@ jQuery(document).ready(function($) {
     $('.first-pit-help').hide();
   }
   default_ecosystem_type_in_add_mode();
-  
+  //Note these need changing when the sort order changes
   function default_ecosystem_type_in_add_mode() {
-    $('#sc\\:adult-soil-0\\:\\:occAttr\\:538').val(6106);
+    $('#sc\\:adult-soil-0\\:\\:occAttr\\:538').val(6105);
     $('#sc\\:adult-soil-1\\:\\:occAttr\\:538').val(6107);
-    $('#sc\\:adult-soil-2\\:\\:occAttr\\:538').val(6105);
+    $('#sc\\:adult-soil-2\\:\\:occAttr\\:538').val(6106);
     
-    $('#sc\\:immature-soil-0\\:\\:occAttr\\:538').val(6106);
+    $('#sc\\:immature-soil-0\\:\\:occAttr\\:538').val(6105);
     $('#sc\\:immature-soil-1\\:\\:occAttr\\:538').val(6107);
-    $('#sc\\:immature-soil-2\\:\\:occAttr\\:538').val(6105);
+    $('#sc\\:immature-soil-2\\:\\:occAttr\\:538').val(6106);
     
-    $('#sc\\:adult-mustard-0\\:\\:occAttr\\:538').val(6106);
+    $('#sc\\:adult-mustard-0\\:\\:occAttr\\:538').val(6105);
     $('#sc\\:adult-mustard-1\\:\\:occAttr\\:538').val(6107);
-    $('#sc\\:adult-mustard-2\\:\\:occAttr\\:538').val(6105);
+    $('#sc\\:adult-mustard-2\\:\\:occAttr\\:538').val(6106);
     
-    $('#sc\\:immature-mustard-0\\:\\:occAttr\\:538').val(6106);
+    $('#sc\\:immature-mustard-0\\:\\:occAttr\\:538').val(6105);
     $('#sc\\:immature-mustard-1\\:\\:occAttr\\:538').val(6107);
-    $('#sc\\:immature-mustard-2\\:\\:occAttr\\:538').val(6105);
+    $('#sc\\:immature-mustard-2\\:\\:occAttr\\:538').val(6106);
   }
 });
 
