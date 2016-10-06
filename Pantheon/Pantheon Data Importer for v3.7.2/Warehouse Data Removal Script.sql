e04d979c6692dc46a79f7a25aa4b69a61cb34730
John van Breda <john.vanbreda@biodiverseit.co.uk>
2016-10-06 09:47:57 +0100

    Cleanup script fixes
    * Changed way of selecting attribute IDs so it works with current state of data
    * A couple of SQL tidyups
    * Marked temporary tables as such.

--- Pantheon/Pantheon Data Importer for v3.7.2/Warehouse Data Removal Script.sql
+++ Pantheon/Pantheon Data Importer for v3.7.2/Warehouse Data Removal Script.sql
@@ -4,34 +4,38 @@
 
 set search_path TO indicia, public;
 
---select taxa_taxon_list_attribute_id into taxon_lists_taxa_taxon_list_attributes_removal_rows
-from indicia.taxon_lists_taxa_taxon_list_attributes
-where deleted = false AND taxon_list_id = <pantheon_taxon_list_id>;
+select id into temporary taxa_taxon_list_attributes_removal_rows
+from indicia.taxa_taxon_list_attributes
+where id between 5 and 32;
 
 --update taxa_taxon_list_attribute_values
 set deleted = true 
-where taxa_taxon_list_attribute_id in
-(select id from taxa_taxon_list_attributes where id  in
-(select taxa_taxon_list_attribute_id from taxon_lists_taxa_taxon_list_attributes_removal_rows));
+where taxa_taxon_list_attribute_id in (
+  select id from taxa_taxon_list_attributes_removal_rows
+);
 
 --update taxon_lists_taxa_taxon_list_attributes 
 set deleted = true 
-where deleted = false AND taxon_list_id = <pantheon_taxon_list_id>;
+where deleted = false AND taxa_taxon_list_attribute_id in (
+  select id from taxa_taxon_list_attributes_removal_rows
+);
 
 --update taxa_taxon_list_attributes 
 set deleted = true
-where id in
-(select taxa_taxon_list_attribute_id from taxon_lists_taxa_taxon_list_attributes_removal_rows);
+where id in (
+  select id from taxa_taxon_list_attributes_removal_rows
+);
 
 --Remove temporary table
 drop table taxon_lists_taxa_taxon_list_attributes_removal_rows;
 
-
---select id, term_id, termlist_id into termlists_terms_removal_rows
+--select id, term_id, termlist_id into temporary termlists_terms_removal_rows
 from indicia.termlists_terms
-where termlist_id in
-(select id from termlists where deleted = false AND website_id in
-(select id from websites where title = 'Pantheon' and deleted = false));
+where termlist_id in (
+  select t.id from termlists t 
+  join websites w on w.id=t.website_id and w.title='Pantheon' and w.deleted=false
+  where t.deleted = false
+);
 
 --update termlists_terms 
 set deleted = true
