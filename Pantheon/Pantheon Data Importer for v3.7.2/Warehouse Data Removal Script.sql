100d023f185310d3d115b8920cfc1659d419c5d3
John van Breda <john.vanbreda@biodiverseit.co.uk>
2016-12-21 11:51:26 +0000

    Updated script to do a true cleanup

--- Pantheon/Pantheon Data Importer for v3.7.2/Warehouse Data Removal Script.sql
+++ Pantheon/Pantheon Data Importer for v3.7.2/Warehouse Data Removal Script.sql
@@ -1,56 +1,45 @@
--- Instructions for user
--- Firstly find and replace this tag in the script <pantheon_taxon_list_id>
--- Secondly uncomment all the first lines in the statements (prevents accidental use)
-
 set search_path TO indicia, public;
 
-select id into temporary taxa_taxon_list_attributes_removal_rows
-from indicia.taxa_taxon_list_attributes
-where id between 5 and 32;
+select id 
+into temporary taxa_taxon_list_attributes_removal_rows
+from taxa_taxon_list_attributes
+where id between 158 and 183;
 
---update taxa_taxon_list_attribute_values
-set deleted = true 
-where taxa_taxon_list_attribute_id in (
+delete from taxa_taxon_list_attribute_values where taxa_taxon_list_attribute_id in (
   select id from taxa_taxon_list_attributes_removal_rows
 );
 
---update taxon_lists_taxa_taxon_list_attributes 
-set deleted = true 
-where deleted = false AND taxa_taxon_list_attribute_id in (
+delete from taxon_lists_taxa_taxon_list_attributes where taxa_taxon_list_attribute_id in (
   select id from taxa_taxon_list_attributes_removal_rows
 );
 
---update taxa_taxon_list_attributes 
-set deleted = true
-where id in (
+delete from taxa_taxon_list_attributes where id in (
   select id from taxa_taxon_list_attributes_removal_rows
 );
 
 --Remove temporary table
-drop table taxon_lists_taxa_taxon_list_attributes_removal_rows;
+drop table taxa_taxon_list_attributes_removal_rows;
 
---select id, term_id, termlist_id into temporary termlists_terms_removal_rows
-from indicia.termlists_terms
+select id, term_id, termlist_id into temporary termlists_terms_removal_rows
+from termlists_terms
 where termlist_id in (
   select t.id from termlists t 
   join websites w on w.id=t.website_id and w.title='Pantheon' and w.deleted=false
   where t.deleted = false
 );
 
---update termlists_terms 
-set deleted = true
-where id in
+update termlists_terms set parent_id=null where parent_id in (select id from termlists_terms_removal_rows);
+
+delete from cache_termlists_terms where id in
 (select id from termlists_terms_removal_rows);
 
---update termlists
-set deleted = true
-where id in
+delete from termlists_terms where id in
+(select id from termlists_terms_removal_rows);
+
+delete from termlists where id in
 (select termlist_id from termlists_terms_removal_rows);
 
---update terms 
-set deleted = true
-where id in
-(select term_id from termlists_terms_removal_rows);
+delete from terms where id not in (select term_id from termlists_terms);
 
 --Remove temporary table
 drop table termlists_terms_removal_rows;
