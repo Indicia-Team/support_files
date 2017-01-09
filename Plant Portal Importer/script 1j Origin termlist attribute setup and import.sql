eaa28f9274537d3ebb8699005040ac969cfdbd58
Andrew van Breda <andrew_van_breda@hotmail.com>
2017-01-09 13:15:03 +0000

    Plant Portal Plant Att Importer - Fixed a problem where the origin field import didn't work as it is a comma separated list which needs its terms separating

--- Plant Portal Importer/script 1j Origin termlist attribute setup and import.sql
+++ Plant Portal Importer/script 1j Origin termlist attribute setup and import.sql
@@ -14,220 +14,102 @@ BEGIN
 update plant_portal.tbl_plant_att
 set origin=replace(origin,'?','');
 
---Can't use comma separation, so replace with | as some of the origin names actually contain commas.
---Get rid of the spaces in the origin data as it is processed, this also helps us know which names we have swapped for the longer names
---e.g. "AS" would actually match Asia in a longer name and try a replacement when we don't want it to. However we can avoid this if we also detect if there is a space after the , or |
---then we know which items haven't been processed
+--These codes are problematic if try and replace in the real expanded terms. For instance, the code "Am" would appear in many of the expanded
+--terms also. We only want to do replacements for the codes, not parts of words in the full expanded terms.
+--We can overcome this issue by changing the codes to be properly unique such that they would never appear in real words and such that they
+--don't clash with each other either.
+--Note that the ordering of the processing is important e.g. "SAm" is processed early to avoid clashing with "Am"
 update plant_portal.tbl_plant_att
-set origin=replace(origin,'Am4,','Western North America|');
-update plant_portal.tbl_plant_att
-set origin=replace(origin,'Am4|','Western North America|');
+set origin=replace(origin,'SAm','S:A:m');
 
 update plant_portal.tbl_plant_att
-set origin=replace(origin,', Am4','|Western North America');
-update plant_portal.tbl_plant_att
-set origin=replace(origin,'| Am4','|Western North America');
+set origin=replace(origin,'Am4','A;m;4');
 
 update plant_portal.tbl_plant_att
-set origin='Western North America'
-where origin='Am4';
+set origin=replace(origin,'Am6','A\m\6');
 
 update plant_portal.tbl_plant_att
-set origin=replace(origin,'Am6,','Eastern North America|');
-update plant_portal.tbl_plant_att
-set origin=replace(origin,'Am6|','Eastern North America|');
+set origin=replace(origin,'Am','A/m');
 
 update plant_portal.tbl_plant_att
-set origin=replace(origin,', Am6','|Eastern North America');
-update plant_portal.tbl_plant_att
-set origin=replace(origin,'| Am6','|Eastern North America');
+set origin=replace(origin,'As1','A>s>1');
 
 update plant_portal.tbl_plant_att
-set origin='Eastern North America'
-where origin='Am6';
+set origin=replace(origin,'As2','A<s<2');
 
 update plant_portal.tbl_plant_att
-set origin=replace(origin,'Am,','North America|');
-update plant_portal.tbl_plant_att
-set origin=replace(origin,'Am|','North America|');
+set origin=replace(origin,'As','A.s');
 
 update plant_portal.tbl_plant_att
-set origin=replace(origin,', Am','|North America');
-update plant_portal.tbl_plant_att
-set origin=replace(origin,'| Am','|North America');
+set origin=replace(origin,'Aus','A-u-s');
 
 update plant_portal.tbl_plant_att
-set origin='North America'
-where origin='Am';
+set origin=replace(origin,'Crop','C{r{o{p');
 
 update plant_portal.tbl_plant_att
-set origin=replace(origin,'As1,','Asia between 60°E and 120°E|');
-update plant_portal.tbl_plant_att
-set origin=replace(origin,'As1|','Asia between 60°E and 120°E|');
+set origin=replace(origin,'Eur','E}u}r');
 
 update plant_portal.tbl_plant_att
-set origin=replace(origin,', As1','|Asia between 60°E and 120°E');
-update plant_portal.tbl_plant_att
-set origin=replace(origin,'| As1','|Asia between 60°E and 120°E');
+set origin=replace(origin,'Gard','G"a"r"d');
 
 update plant_portal.tbl_plant_att
-set origin='Asia between 60°E and 120°E'
-where origin='As1';
+set origin=replace(origin,'NHem','N?H?e?m');
 
 update plant_portal.tbl_plant_att
-set origin=replace(origin,'As2,','Asia E of 120°E|');
-update plant_portal.tbl_plant_att
-set origin=replace(origin,'As2|','Asia E of 120°E|');
+set origin=replace(origin,'NZ','N[Z');
 
 update plant_portal.tbl_plant_att
-set origin=replace(origin,', As2','|Asia E of 120°E');
-update plant_portal.tbl_plant_att
-set origin=replace(origin,'| As2','|Asia E of 120°E');
+set origin=replace(origin,'SAf','S]A]f');
 
 update plant_portal.tbl_plant_att
-set origin='Asia E of 120°E'
-where origin='As2';
+set origin=replace(origin,'Unk','U#n#k');
+
+
 
 update plant_portal.tbl_plant_att
-set origin=replace(origin,'As,','Asia east of 60°E|');
-update plant_portal.tbl_plant_att
-set origin=replace(origin,'As|','Asia east of 60°E|');
+set origin=replace(origin,'S:A:m','South America and/or Central America');
 
 update plant_portal.tbl_plant_att
-set origin=replace(origin,', As','|Asia east of 60°E');
-update plant_portal.tbl_plant_att
-set origin=replace(origin,'| As','|Asia east of 60°E');
+set origin=replace(origin,'A;m;4','Western North America');
 
 update plant_portal.tbl_plant_att
-set origin='Asia east of 60°E'
-where origin='As';
+set origin=replace(origin,'A\m\6','Eastern North America');
 
 update plant_portal.tbl_plant_att
-set origin=replace(origin,'Aus,','Australia|');
-update plant_portal.tbl_plant_att
-set origin=replace(origin,'Aus|','Australia|');
+set origin=replace(origin,'A/m','North America');
 
 update plant_portal.tbl_plant_att
-set origin=replace(origin,', Aus','|Australia');
-update plant_portal.tbl_plant_att
-set origin=replace(origin,'| Aus','|Australia');
+set origin=replace(origin,'A>s>1','Asia between 60°E and 120°E');
 
 update plant_portal.tbl_plant_att
-set origin='Australia'
-where origin='Aus';
+set origin=replace(origin,'A<s<2','Asia E of 120°E');
 
 update plant_portal.tbl_plant_att
-set origin=replace(origin,'Crop,','Crop plant, does not have a native range|');
-update plant_portal.tbl_plant_att
-set origin=replace(origin,'Crop|','Crop plant, does not have a native range|');
+set origin=replace(origin,'A.s','Asia east of 60°E');
 
 update plant_portal.tbl_plant_att
-set origin=replace(origin,', Crop','|Crop plant, does not have a native range');
-update plant_portal.tbl_plant_att
-set origin=replace(origin,'| Crop','|Crop plant, does not have a native range');
+set origin=replace(origin,'A-u-s','Australia');
 
 update plant_portal.tbl_plant_att
-set origin='Crop plant, does not have a native range'
-where origin='Crop';
+set origin=replace(origin,'C{r{o{p','Crop plant| does not have a native range');
 
 update plant_portal.tbl_plant_att
-set origin=replace(origin,'Eur,','Europe|');
-update plant_portal.tbl_plant_att
-set origin=replace(origin,'Eur|','Europe|');
+set origin=replace(origin,'E}u}r','Europe');
 
 update plant_portal.tbl_plant_att
-set origin=replace(origin,', Eur','|Europe');
-update plant_portal.tbl_plant_att
-set origin=replace(origin,'| Eur','|Europe');
+set origin=replace(origin,'G"a"r"d','Garden origin| does not have a native range');
 
 update plant_portal.tbl_plant_att
-set origin='Europe'
-where origin='Eur';
+set origin=replace(origin,'N?H?e?m','N Hemisphere (Europe|Asia and North America)');
 
 update plant_portal.tbl_plant_att
-set origin=replace(origin,'Gard,','Garden origin, does not have a native range|');
-update plant_portal.tbl_plant_att
-set origin=replace(origin,'Gard|','Garden origin, does not have a native range|');
+set origin=replace(origin,'N[Z','New Zealand');
 
 update plant_portal.tbl_plant_att
-set origin=replace(origin,', Gard','|Garden origin, does not have a native range');
-update plant_portal.tbl_plant_att
-set origin=replace(origin,'| Gard','|Garden origin, does not have a native range');
+set origin=replace(origin,'S]A]f','Southern Africa');
 
 update plant_portal.tbl_plant_att
-set origin='Garden origin, does not have a native range'
-where origin='Gard';
-
-update plant_portal.tbl_plant_att
-set origin=replace(origin,'NHem,','N Hemisphere (Europe,Asia and North America)|');
-update plant_portal.tbl_plant_att
-set origin=replace(origin,'NHem|','N Hemisphere (Europe,Asia and North America)|');
-
-update plant_portal.tbl_plant_att
-set origin=replace(origin,', NHem','|N Hemisphere (Europe,Asia and North America)');
-update plant_portal.tbl_plant_att
-set origin=replace(origin,'| NHem','|N Hemisphere (Europe,Asia and North America)');
-
-update plant_portal.tbl_plant_att
-set origin='N Hemisphere (Europe,Asia and North America)'
-where origin='NHem';
-
-update plant_portal.tbl_plant_att
-set origin=replace(origin,'NZ,','New Zealand|');
-update plant_portal.tbl_plant_att
-set origin=replace(origin,'NZ|','New Zealand|');
-
-update plant_portal.tbl_plant_att
-set origin=replace(origin,', NZ','|New Zealand');
-update plant_portal.tbl_plant_att
-set origin=replace(origin,'| NZ','|New Zealand');
-
-update plant_portal.tbl_plant_att
-set origin='New Zealand'
-where origin='NZ';
-
-update plant_portal.tbl_plant_att
-set origin=replace(origin,'SAf,','Southern Africa|');
-update plant_portal.tbl_plant_att
-set origin=replace(origin,'SAf|','Southern Africa|');
-
-update plant_portal.tbl_plant_att
-set origin=replace(origin,', SAf','|Southern Africa');
-update plant_portal.tbl_plant_att
-set origin=replace(origin,'| SAf','|Southern Africa');
-
-update plant_portal.tbl_plant_att
-set origin='Southern Africa'
-where origin='SAf';
-
-update plant_portal.tbl_plant_att
-set origin=replace(origin,'SAm,','South America and/or Central America|');
-update plant_portal.tbl_plant_att
-set origin=replace(origin,'SAm|','South America and/or Central America|');
-
-update plant_portal.tbl_plant_att
-set origin=replace(origin,', SAm','|South America and/or Central America');
-update plant_portal.tbl_plant_att
-set origin=replace(origin,'| SAm','|South America and/or Central America');
-
-update plant_portal.tbl_plant_att
-set origin='South America and/or Central America'
-where origin='SAm';
-
-update plant_portal.tbl_plant_att
-set origin=replace(origin,'Unk,','Unknown|');
-update plant_portal.tbl_plant_att
-set origin=replace(origin,'Unk|','Unknown|');
-
-update plant_portal.tbl_plant_att
-set origin=replace(origin,', Unk','|Unknown');
-update plant_portal.tbl_plant_att
-set origin=replace(origin,'| Unk','|Unknown');
-
-update plant_portal.tbl_plant_att
-set origin='Unknown'
-where origin='Unk';
-
+set origin=replace(origin,'U#n#k','Unknown');
 
 set search_path TO indicia, public;
 insert into indicia.termlists (title,description,website_id,created_on,created_by_id,updated_on,updated_by_id,external_key)
@@ -248,7 +130,7 @@ where caption='origin'
 ORDER BY id DESC 
 LIMIT 1;
 
-
+perform insert_term('South America and/or Central America','eng',null,'indicia:origin');
 perform insert_term('North America','eng',null,'indicia:origin');
 perform insert_term('Western North America','eng',null,'indicia:origin');
 perform insert_term('Eastern North America','eng',null,'indicia:origin');
@@ -256,13 +138,12 @@ perform insert_term('Asia east of 60°E','eng',null,'indicia:origin');
 perform insert_term('Asia between 60°E and 120°E','eng',null,'indicia:origin');
 perform insert_term('Asia E of 120°E','eng',null,'indicia:origin');
 perform insert_term('Australia','eng',null,'indicia:origin');
-perform insert_term('Crop plant, does not have a native range','eng',null,'indicia:origin');
+perform insert_term('Crop plant| does not have a native range','eng',null,'indicia:origin');
 perform insert_term('Europe','eng',null,'indicia:origin');
-perform insert_term('Garden origin, does not have a native range','eng',null,'indicia:origin');
-perform insert_term('N Hemisphere (Europe,Asia and North America)','eng',null,'indicia:origin');
+perform insert_term('Garden origin| does not have a native range','eng',null,'indicia:origin');
+perform insert_term('N Hemisphere (Europe|Asia and North America)','eng',null,'indicia:origin');
 perform insert_term('New Zealand','eng',null,'indicia:origin');
 perform insert_term('Southern Africa','eng',null,'indicia:origin');
-perform insert_term('South America and/or Central America','eng',null,'indicia:origin');
 perform insert_term('Unknown','eng',null,'indicia:origin');
 
 
@@ -273,7 +154,7 @@ join indicia.taxa it on it.external_key=ppt.preferred_tvk AND it.deleted=false
 join indicia.taxa_taxon_lists ittl on ittl.taxon_id=it.id AND ittl.taxon_list_id=<plant_port_taxon_list_id> AND ittl.deleted=false
 where ppt.origin IS NOT NULL
 ) loop
-  origin_to_split_array = string_to_array(trait_to_import.origin_to_split, '|');
+  origin_to_split_array = string_to_array(trait_to_import.origin_to_split, ',');
   FOR i IN array_lower(origin_to_split_array, 1) .. array_upper(origin_to_split_array, 1)
       LOOP
          --As this is a multi-value attribute, this time we only don't add the trait if there is an exact taxa_taxon_list_id and attribute value match
@@ -284,7 +165,7 @@ where ppt.origin IS NOT NULL
            join termlists_terms itt on itt.id = ttlav2.int_value AND itt.deleted=false
            join termlists itl on itl.id = itt.termlist_id AND itl.title='origin' AND itl.deleted=false
            join websites w on w.id = itl.website_id AND w.title='Plant Portal' AND w.deleted=false
-           join terms t on t.id=itt.term_id AND t.term=origin_to_split_array[i]
+           join terms t on t.id=itt.term_id AND t.term=trim(origin_to_split_array[i])
            where ttlav2.taxa_taxon_list_attribute_id = (select id from taxa_taxon_list_attributes where caption='origin' and deleted=false order by id desc limit 1) AND ttlav2.deleted=false
            ORDER BY ttlav2.id desc))
          THEN
@@ -308,7 +189,7 @@ where ppt.origin IS NOT NULL
              from indicia.termlists_terms itt 
              join termlists itl on itl.id = itt.termlist_id AND itl.title='origin' AND itl.deleted=false
              join websites w on w.id = itl.website_id AND w.title='Plant Portal' AND w.deleted=false
-             join terms t on t.id=itt.term_id AND t.term=origin_to_split_array[i]),
+             join terms t on t.id=itt.term_id AND t.term=trim(origin_to_split_array[i])),
              1,
              now(),
              1,
@@ -322,16 +203,20 @@ END LOOP;
 END
 $do$;
 
+--Note as the lists of terms are separated using commas, if we use a pipe symbol inside terms
+--where commas are normally used we can avoid replacements in the wrong place.
+--However these these pipe symbols now need changing at the end
+update terms 
+set term = 'Crop plant, does not have a native range'
+where term = 'Crop plant| does not have a native range';
 
+update terms
+set term = 'Garden origin, does not have a native range'
+where term = 'Garden origin| does not have a native range';
 
-
-
-
-
-
-
-
-
+update terms
+set term = 'N Hemisphere (Europe,Asia and North America)'
+where term = 'N Hemisphere (Europe|Asia and North America)';
 
 
 
