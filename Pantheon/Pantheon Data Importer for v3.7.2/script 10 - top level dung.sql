94065d1ad66a9f4d6465fae49ca13f94d9666e4e
John van Breda <john.vanbreda@biodiverseit.co.uk>
2017-02-17 15:47:29 +0000

    Include sub-levels under top-level dung & carrion
    Fixes https://github.com/BiologicalRecordsCentre/Pantheon/issues/94

--- Pantheon/Pantheon Data Importer for v3.7.2/script 10 - top level dung.sql
+++ Pantheon/Pantheon Data Importer for v3.7.2/script 10 - top level dung.sql
@@ -29,5 +29,33 @@ left join (
 where av2.id is null
 order by cttl.preferred_taxon
 
-
-
+insert into taxa_taxon_list_attribute_values (
+  taxa_taxon_list_id,
+  taxa_taxon_list_attribute_id,
+  int_value,
+  created_on, 
+  created_by_id,
+  updated_on,
+  updated_by_id
+)
+select 
+  cttl.preferred_taxa_taxon_list_id,
+  (select id from taxa_taxon_list_attributes where caption='resource'),  
+  (select dtt.id from detail_termlists_terms dtt 
+    join cache_termlists_terms dttp on dttp.id=dtt.parent_id and dttp.parent_id is null
+    where dtt.term=t.term and dtt.termlist_external_key='indicia:broad biotope/habitat/resource'),
+  now(), 
+  1,
+  now(),
+  1
+from cache_taxa_taxon_lists cttl
+join taxa_taxon_list_attribute_values av on av.taxa_taxon_list_id=cttl.preferred_taxa_taxon_list_id
+join cache_termlists_terms t on t.id=av.int_value and t.term in ('dung','carrion') 
+join cache_termlists_terms tp on tp.id=t.parent_id and tp.parent_id is not null
+left join (
+  taxa_taxon_list_attribute_values av2 
+  join cache_termlists_terms t2 on t2.id=av2.int_value and t2.term in ('dung','carrion') 
+  join cache_termlists_terms tp2 on tp2.id=t2.parent_id and tp2.parent_id is null
+) on av2.taxa_taxon_list_id=cttl.preferred_taxa_taxon_list_id
+where av2.id is null
+order by cttl.preferred_taxon
