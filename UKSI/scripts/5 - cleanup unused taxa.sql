c12dd88be65e2175ce8da4bc68f56118172a03bd
John van Breda <john.vanbreda@biodiverseit.co.uk>
2018-01-11 16:05:30 +0000

    Removes FK violation if taxa to be deleted point to designations still.

--- UKSI/scripts/5 - cleanup unused taxa.sql
+++ UKSI/scripts/5 - cleanup unused taxa.sql
@@ -1,6 +1,10 @@
+SET search_path=indicia, public;
+
 -- Just in case, remove any taxa which don't have a taxa_taxon_list record.
 DELETE FROM taxa tdel
 USING taxa t
 LEFT JOIN taxa_taxon_lists ttl ON ttl.taxon_id=t.id
+LEFT JOIN taxa_taxon_designations ttd ON ttd.taxon_id=t.id
 WHERE t.id=tdel.id
 AND ttl.id IS NULL
+AND ttd.id IS NULL;
