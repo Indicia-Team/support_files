b3ab426043e5e6389b11ab1bc5ce3ff66038b908
John van Breda <john.vanbreda@biodiverseit.co.uk>
2018-01-23 21:58:39 +0000

    Should not use cache_* tables at this point as may not be up to date.

--- UKSI/scripts/36 - ensure cache update is thorough.sql
+++ UKSI/scripts/36 - ensure cache update is thorough.sql
@@ -26,11 +26,13 @@ INSERT INTO uksi.changed_taxa_taxon_list_ids(
 
 -- Build a complete hierarchy so we can spot changes required in the cache tables
 WITH RECURSIVE q AS (
-  SELECT distinct ttl1.id AS child_id, ttl1.taxon AS child_taxon, ttlpref.parent_id,
+  SELECT distinct ttl1.id AS child_id, t1.taxon AS child_taxon, ttlpref.parent_id,
       ttlpref.id AS rank_ttl_id, t.taxon AS rank_taxon, tr.rank, tr.id AS taxon_rank_id, tr.sort_order AS taxon_rank_sort_order
-  FROM cache_taxa_taxon_lists ttl1
-  JOIN cache_taxa_taxon_lists ttlpref ON ttlpref.external_key=ttl1.external_key
-    AND ttlpref.taxon_list_id=(SELECT uksi_taxon_list_id FROM uksi.uksi_settings) AND ttlpref.preferred=true AND ttlpref.allow_data_entry=true
+  FROM taxa_taxon_lists ttl1
+  JOIN taxa t1 ON t1.id=ttl1.taxon_id AND t1.deleted=false
+  JOIN taxa t1pref ON t1pref.search_code=t1.external_key and tlpref.deleted=false
+  JOIN taxa_taxon_lists ttlpref ON ttlpref.taxon_id=t1.id AND ttlpref.deleted=false
+    AND ttlpref.taxon_list_id=ttl1.taxon_list_id AND ttlpref.preferred=true AND ttlpref.allow_data_entry=true
   JOIN taxa_taxon_lists ttlprefraw ON ttlprefraw.id=ttlpref.id AND ttlprefraw.deleted=false
   JOIN taxa t ON t.id=ttlprefraw.taxon_id AND t.deleted=false AND t.deleted=false
   JOIN taxon_ranks tr ON tr.id=t.taxon_rank_id AND tr.deleted=false AND tr.deleted=false
