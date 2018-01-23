9d992e9166114199e872c9d97c13a502d3837245
John van Breda <john.vanbreda@biodiverseit.co.uk>
2018-01-23 08:50:20 +0000

    Ensure that parent links point to preferred names.

--- UKSI/scripts/27 - populate parent links.sql
+++ UKSI/scripts/27 - populate parent links.sql
@@ -3,13 +3,17 @@ SET search_path=indicia, public;
 -- Grab the ID of the parent taxa_taxon_list record accoring to the parent_tvk. Use the orig_parent_id to
 -- detect if it's an actual change.
 UPDATE uksi.prepared_taxa_taxon_lists pttl
-SET parent_id=pttlp.id,
-  changed=pttl.changed OR pttlp.id<>COALESCE(pttl.orig_parent_id, 0)
+SET parent_id=pttlpref.id,
+  changed=pttl.changed OR pttlpref.id<>COALESCE(pttl.orig_parent_id, 0)
 FROM taxa tp
 JOIN uksi.prepared_taxa_taxon_lists pttlp
   ON pttlp.taxon_id=tp.id
+JOIN uksi.prepared_taxa_taxon_lists pttlpref
+  ON pttlpref.taxon_meaning_id=pttlp.taxon_meaning_id
+  AND pttlpref.preferred=true
 WHERE tp.search_code=pttl.parent_search_code
-AND pttlp.taxon_list_id=pttl.taxon_list_id;
+AND pttlp.taxon_list_id=pttl.taxon_list_id
+AND pttlpref.taxon_list_id=pttl.taxon_list_id;
 
 -- Ensure the parent ID is clear where it should be.
 UPDATE uksi.prepared_taxa_taxon_lists pttl
