2604bfc1b9487389bb4d71b963311d4b609c5271
John van Breda <john.vanbreda@biodiverseit.co.uk>
2018-01-25 11:32:22 +0000

    Corrected boolean test - want to update if different, not same

--- UKSI/scripts/36 - ensure cache update is thorough.sql
+++ UKSI/scripts/36 - ensure cache update is thorough.sql
@@ -98,7 +98,7 @@ FROM cache_taxa_taxon_lists cttl
 WHERE cttl.id=u.taxa_taxon_list_id
 AND (
   COALESCE(u.taxon_rank_sort_order, 0)<>COALESCE(cttl.taxon_rank_sort_order, 0)
-  OR COALESCE(u.family_taxa_taxon_list_id, 0)=COALESCE(cttl.family_taxa_taxon_list_id, 0)
+  OR COALESCE(u.family_taxa_taxon_list_id, 0)<>COALESCE(cttl.family_taxa_taxon_list_id, 0)
 );
 
 DROP TABLE rankupdate;
