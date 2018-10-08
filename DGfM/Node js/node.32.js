320600de991d583241b9677d492dc5cfa72385aa
Andrew van Breda <support@avb-it.co.uk>
2018-10-08 12:59:28 +0100

    - DGFM data entry page colour picker javascript - Also the Edit Fungi page and Species lookup pages warn the user if they try to search before entering a species.

--- DGfM/Node js/node.32.js
+++ DGfM/Node js/node.32.js
@@ -1,6 +1,14 @@
 jQuery(document).ready(function($) {
   $('#submit-to-edit-page').submit(function () {
+    if (!$('#occurrence\\:taxa_taxon_list_id').val()) {
+      alert('Please enter a species to edit');
+      return false;
+    }  
     $('#occurrence\\:taxa_taxon_list_id\\:taxon').prop('name', '');
     $('#occurrence\\:taxa_taxon_list_id').prop('name', 'taxa_taxon_list_id');
   });
 });
+
+
+ 
+ 
