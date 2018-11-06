3ee84210b273352c7c8b13ba83247da0dc545ebb
Andrew van Breda <support@avb-it.co.uk>
2018-11-06 13:11:07 +0000

    Improvements  to positioning of items on the page. Journal/book area now has the title at the top of the sub-area. Length/Width now at top of the sub-area. Comment fields positioned on the page correctly.

--- DGfM/Node js/node.23.js
+++ DGfM/Node js/node.23.js
@@ -1,4 +1,87 @@
 jQuery(document).ready(function($) {
+  $('#ctrl-wrap-taxAttr-218').insertBefore('#ctrl-wrap-taxAttr-205');
+  $('#ctrl-wrap-taxAttr-861').insertAfter('#ctrl-wrap-taxAttr-218');
+  $('#ctrl-wrap-taxAttr-1075').insertAfter('#ctrl-wrap-taxAttr-37');
+  //Position Journal/book at top of sub-areas
+  $('#ctrl-wrap-taxAttr-942').insertBefore('#ctrl-wrap-taxAttr-898');
+  $('#ctrl-wrap-taxAttr-945').insertBefore('#ctrl-wrap-taxAttr-901');
+  $('#ctrl-wrap-taxAttr-23').insertBefore('#ctrl-wrap-taxAttr-25');
+  $('#ctrl-wrap-taxAttr-833').insertBefore('#ctrl-wrap-taxAttr-904');
+  $('#ctrl-wrap-taxAttr-836').insertBefore('#ctrl-wrap-taxAttr-741');
+  
+  //Place title field after Journal/Book
+  $('#ctrl-wrap-taxAttr-794').insertAfter('#ctrl-wrap-taxAttr-942');
+  $('#ctrl-wrap-taxAttr-944').insertAfter('#ctrl-wrap-taxAttr-945');
+  $('#ctrl-wrap-taxAttr-22').insertAfter('#ctrl-wrap-taxAttr-23');
+  $('#ctrl-wrap-taxAttr-832').insertAfter('#ctrl-wrap-taxAttr-833');
+  $('#ctrl-wrap-taxAttr-835').insertAfter('#ctrl-wrap-taxAttr-836');
+  
+  //Place Length and Width at top of sub-areas for English
+  $('#ctrl-wrap-taxAttr-908').insertAfter("legend:contains('Basidium'),legend:contains('Basidien'),legend:contains('bazidie')");
+  $('#ctrl-wrap-taxAttr-951').insertAfter('#ctrl-wrap-taxAttr-908');
+  $('#ctrl-wrap-taxAttr-369').insertAfter("legend:contains('chlamydospores'),legend:contains('Chlamydosporen'),legend:contains('chlamydospóry')");
+  $('#ctrl-wrap-taxAttr-978').insertAfter('#ctrl-wrap-taxAttr-369');
+  $('#ctrl-wrap-taxAttr-411').insertAfter("legend:contains('cystidioles'),legend:contains('Cystidiolen'),legend:contains('cystidioly')");
+  $('#ctrl-wrap-taxAttr-410').insertAfter('#ctrl-wrap-taxAttr-411');
+  $('#ctrl-wrap-taxAttr-454').insertAfter("legend:contains('gloeocystidium'),legend:contains('Gloeocystiden'),legend:contains('gloeocystidy')");
+  $('#ctrl-wrap-taxAttr-990').insertAfter('#ctrl-wrap-taxAttr-454');
+  $('#ctrl-wrap-taxAttr-477').insertAfter("legend:contains('hairs/setae'),legend:contains('Haare/Setae'),legend:contains('chlupy/sety')");
+  $('#ctrl-wrap-taxAttr-458').insertAfter('#ctrl-wrap-taxAttr-477');
+  $('#ctrl-wrap-taxAttr-1001').insertAfter("legend:contains('hymenial cystidium'),legend:contains('Hymenialzystiden'),legend:contains('hymeniální cystidy')");
+  $('#ctrl-wrap-taxAttr-1000').insertAfter('#ctrl-wrap-taxAttr-1001');
+  $('#ctrl-wrap-taxAttr-1005').insertAfter("legend:contains('lagenocystidia'),legend:contains('Lagenozystiden'),legend:contains('lagenocystidy')");
+  $('#ctrl-wrap-taxAttr-1004').insertAfter('#ctrl-wrap-taxAttr-1005');
+  $('#ctrl-wrap-taxAttr-1011').insertAfter("legend:contains('Lamprocystidia'),legend:contains('Lamprocystiden'),legend:contains('lamprocystidy')");
+  $('#ctrl-wrap-taxAttr-1010').insertAfter('#ctrl-wrap-taxAttr-1011');
+  $('#ctrl-wrap-taxAttr-29').insertAfter("legend:contains('Leptocystidia'),legend:contains('Leptocystiden'),legend:contains('leptocystidy')");
+  $('#ctrl-wrap-taxAttr-1013').insertAfter('#ctrl-wrap-taxAttr-29');
+  $('#ctrl-wrap-taxAttr-1016').insertAfter("legend:contains('lyocystidy'),legend:contains('Lyocystiden'),legend:contains('leptocystidy')");
+  $('#ctrl-wrap-taxAttr-538').insertAfter('#ctrl-wrap-taxAttr-1016');
+  $('#ctrl-wrap-taxAttr-1020').insertAfter("legend:contains('metuloid cystidia'),legend:contains('Metuloide Cystiden'),legend:contains('metuloidní cystidy')");
+  $('#ctrl-wrap-taxAttr-1019').insertAfter('#ctrl-wrap-taxAttr-1020');
+  $('#ctrl-wrap-taxAttr-1042').insertAfter("legend:contains('septocystidia'),legend:contains('Septocystiden'),legend:contains('septocystidy')");
+  $('#ctrl-wrap-taxAttr-1041').insertAfter('#ctrl-wrap-taxAttr-1042');
+  $('#ctrl-wrap-taxAttr-1031').insertAfter("legend:contains('Pleurocystidia'),legend:contains('Pleurocystiden'),legend:contains('pleurocystidy')");
+  $('#ctrl-wrap-taxAttr-1030').insertAfter('#ctrl-wrap-taxAttr-1031');
+  $('#ctrl-wrap-taxAttr-1062').insertAfter("legend:contains('trama cystidia'),legend:contains('Tramacystiden'),legend:contains('tramální cystidy')");
+  $('#ctrl-wrap-taxAttr-1061').insertAfter('#ctrl-wrap-taxAttr-1062');
+  //$('#ctrl-wrap-taxAttr-999').insertAfter("legend:contains('Trama of cap')");
+  //$('#ctrl-wrap-taxAttr-998').insertAfter('#ctrl-wrap-taxAttr-999');
+
+
+  //Position comment fields
+  //Note for each array element, the first item is the main attribute id, the second id is the comment field
+  var comment_field_selectors = [
+    [1084,39],[1085,40],[1086,41],[1087,47],[1088,48],[1089,55],[1090,57],[1091,58],[1092,59],[1093,62],
+    [1094,63],[1095,69],[1096,70],[1103,84],[1104,85],[1105,86],[1106,87],[1107,88],[1108,89],[1109,90],
+    [1110,93],[1111,94],[1111,94],[1112,96],[1113,97],[1114,98],[1115,99],[1116,100],[1117,102],[1118,103],
+    [1119,105],[1120,111],[1121,113],[1122,114],[1123,115],[1124,116],[1125,117],[1126,119],[1127,120],
+    [1128,121],[1129,122],[1130,123],[1131,126],[1132,128],[1133,131],[1134,132],[1135,133],[1136,134],
+    [1137,135],[1138,136],[1139,137],[1140,138],[1141,139],[1142,140],[1143,144],[1144,151],[1146,153],
+    [1147,154],[1148,156],[1149,167],[1150,171],[1151,172],[1152,173],[1155,185],[1156,187],[1157,188],
+    [1158,190],[1159,191],[1160,192],[1161,195],[1162,196],[1163,205],[1164,206],[1165,207],[1166,209],
+    [1167,210],[1168,211],[1169,212],[1170,213],[1171,214],[1173,216],[1174,217],[1175,221],[1176,222],
+    [1177,225],[1178,226],[1179,227],[1180,228],[1181,229],[1182,232],[1183,233],[1184,234],[1185,236],
+    [1186,238],[1187,239],[1188,247],[1189,248],[1190,251],[1191,252],[1192,253],[1193,255],[1194,256],
+    [1195,258],[1196,269],[1197,270],[1198,274],[1199,277],[1200,278],[1201,279],[1202,280],[1203,283],
+    [1204,292],[1205,293],[1206,294],[1207,296],[1208,300],[1209,302],[1210,305],[1211,306],[1212,307],
+    [1213,309],[1214,321],[1215,327],[1216,339],[1217,340],[1218,341],[1219,342],[1220,354],[1221,362],
+    [1222,366],[1223,367],[1224,377],[1225,390],[1226,391],[1227,392],[1228,399],[1229,426],[1230,429],
+    [1231,432],[1232,440],[1234,444],[1235,445],[1236,446],[1237,449],[1238,452],[1239,459],[1240,466],
+    [1241,468],[1242,469],[1243,474],[1244,475],[1245,481],[1246,484],[1247,486],[1248,489],[1249,491],
+    [1250,503],[1251,508],[1252,510],[1253,513],[1254,525],[1255,529],[1256,530],[1257,533],[1258,542],
+    [1259,545],[1260,546],[1261,549],[1262,567],[1263,568],[1264,569],[1265,578],[1266,579],[1267,580],
+    [1268,581],[1269,586],[1270,589],[1271,590],[1272,600],[1273,602],[1274,607],[1276,620],[1277,622],
+    [1278,626],[1279,628],[1280,630],[1281,632],[1282,634],[1283,639],[1284,641],[1285,642],[1286,644],
+    [1287,646],[1288,648],[1289,650],[1290,652],[1291,656],[1292,658],[1293,670],[1294,676],[1295,693],
+    [1297,701],[1298,703],[1299,704],[1300,712],[1303,717],[1304,728],[1305,750],[1306,751],[1307,756],
+    [1308,758],[1309,759],[1310,760],[1311,763],[1312,767],[1313,768]
+  ];
+
+  $.each(comment_field_selectors, function() {
+    $('#ctrl-wrap-taxAttr-'+this[0]).insertAfter('#ctrl-wrap-taxAttr-'+this[1]);
+  });
+
   var sitePath = '/';
   var spectrumPath = sitePath + 'sites/default/files/indicia/js/spectrum/';
   var attrs = [
