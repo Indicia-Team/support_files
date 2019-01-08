908724060fead1b13a239a45c3b8562fd436e2a0
John van Breda <john.vanbreda@biodiverseit.co.uk>
2019-01-08 12:32:45 +0000

    Fix duplication in taxon paths

--- Elasticsearch/queries/prepare-taxon-paths.sql
+++ Elasticsearch/queries/prepare-taxon-paths.sql
@@ -1,9 +1,10 @@
-select cttl.external_key, string_agg(cttlp.external_key, ',' order by cttlp.taxon_rank_sort_order)
+select cttl.external_key, string_agg(distinct cttlp.external_key, ',' order by cttlp.taxon_rank_sort_order)
 from cache_taxa_taxon_lists cttl
 join cache_taxon_paths ctp on ctp.external_key=cttl.external_key
   and ctp.taxon_list_id=<taxon_list_id>
 join cache_taxa_taxon_lists cttlp on ctp.path @> ARRAY[cttlp.taxon_meaning_id]
   and cttlp.preferred=true
-  AND cttlp.taxon_list_id=<taxon_list_id>
+  and cttlp.taxon_list_id=<taxon_list_id>
 where cttl.taxon_list_id=<taxon_list_id>
-group by cttl.external_key
+and cttl.preferred=true
+group by cttl.external_key;
