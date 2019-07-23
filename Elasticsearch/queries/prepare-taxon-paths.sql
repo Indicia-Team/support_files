6092821c745a059ab7261de567d231d5292ac051
John van Breda <john.vanbreda@biodiverseit.co.uk>
2019-07-23 15:54:15 +0100

    Can't distinct this query
    Unlikely to matter anyway, but distinct doesn’t work if order by a
    different field.

--- Elasticsearch/queries/prepare-taxon-paths.sql
+++ Elasticsearch/queries/prepare-taxon-paths.sql
@@ -1,4 +1,4 @@
-select cttl.external_key, string_agg(distinct cttlp.external_key, ',' order by cttlp.taxon_rank_sort_order)
+select cttl.external_key, string_agg(cttlp.external_key, ',' order by cttlp.taxon_rank_sort_order)
 from cache_taxa_taxon_lists cttl
 join cache_taxon_paths ctp on ctp.external_key=cttl.external_key
   and ctp.taxon_list_id=<taxon_list_id>
