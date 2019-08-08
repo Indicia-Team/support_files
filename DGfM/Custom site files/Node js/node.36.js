5a0a715ccbe3b0044795fca2e084a9f25516d7d0
Andrew van Breda <support@avb-it.co.uk>
2019-08-08 13:22:03 +0100

    Node javascript to allow an administrator to undo the finished flag on a fungus description.

---
+++ DGfM/Custom site files/Node js/node.36.js
@@ -0,0 +1,18 @@
+var reject_completion;
+jQuery(document).ready(function($) {
+  reject_completion = function(ttlav_id) {
+    var r = confirm('Are you sure you want to undo the completion of this fungus?');
+    if (r == true) {
+      var postUrl='http://www.fungi-without-borders.eu/en/iform/ajaxproxy?node=&index=taxon_attribute_value';
+      $.post(postUrl,
+        {"website_id":"2","id":ttlav_id, "deleted":"t"},
+        function (data) {
+          location.reload();
+        },
+        'json'
+      );  
+    } else {
+      return false;
+    }
+  }
+});
