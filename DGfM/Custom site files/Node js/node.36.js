412a08f865e8f5d4aca367b6b0766ef9bd2a07d0
Andrew van Breda <support@avb-it.co.uk>
2019-08-08 13:52:01 +0100

    No functionality changes, reflects slight correction in ajaxaroxy module

--- DGfM/Custom site files/Node js/node.36.js
+++ DGfM/Custom site files/Node js/node.36.js
@@ -3,14 +3,14 @@ jQuery(document).ready(function($) {
   reject_completion = function(ttlav_id) {
     var r = confirm('Are you sure you want to undo the completion of this fungus?');
     if (r == true) {
-      var postUrl='http://www.fungi-without-borders.eu/en/iform/ajaxproxy?node=&index=taxon_attribute_value';
+      var postUrl='http://www.fungi-without-borders.eu/en/iform/ajaxproxy?node=&index=taxa_taxon_list_attribute_value';
       $.post(postUrl,
         {"website_id":"2","id":ttlav_id, "deleted":"t"},
         function (data) {
           location.reload();
         },
         'json'
-      );  
+      );
     } else {
       return false;
     }
