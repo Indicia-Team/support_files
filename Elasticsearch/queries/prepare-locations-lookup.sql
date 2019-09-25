41d61d2b98f83c2785c16081ed935effdd68a714
John van Breda <john.vanbreda@biodiverseit.co.uk>
2019-09-25 12:16:42 +0100

    Fixed so locations without code are indexed properly.
    See https://github.com/BiologicalRecordsCentre/iRecord/issues/671

--- Elasticsearch/queries/prepare-locations-lookup.sql
+++ Elasticsearch/queries/prepare-locations-lookup.sql
@@ -3,4 +3,4 @@ FROM locations l
 JOIN cache_termlists_terms t ON t.id=l.location_type_id
 WHERE l.deleted=false
 AND l.location_type_id in (<indexed_location_type_ids>)
-AND l.code NOT LIKE '%+%';
+AND COALESCE(l.code, '') NOT LIKE '%+%';
