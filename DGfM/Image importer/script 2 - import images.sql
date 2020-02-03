5754a1c3724db6ebca9ab9cddad46edc10a5a1b5
Andrew van Breda <support@avb-it.co.uk>
2020-02-03 11:54:38 +0000

    Two improvements
    1. Use json_build_object (which in particular helps with escaping characters)
    2. The image prefix is no longer hard-coded, and there are extra notes on the use of this.

--- DGfM/Image importer/script 2 - import images.sql
+++ DGfM/Image importer/script 2 - import images.sql
@@ -2,6 +2,11 @@
 -- <taxon_list_id>
 -- <min_row_to_process>
 -- <max_row_to_process>
+-- <filename_prefix> (the supplied image files have typically had an extra prefix in the name in comparison to what is in the spreadsheet's
+-- bildnummer column. Specify what this prefix is using this tag.
+-- e.g Agaricus_altipes_00162_FHampe_hab.jpg is listed in the spreadsheet but the file is actually called Funga_Agaricus_altipes_00162_FHampe_hab.jpg
+-- In this case, replace the <filename_prefix> tag with Funga_
+-- Note that it is assumed all the files in the import batch have the same prefix)
 
 -- Note: This script assumes all the images provided are jpgs
 
@@ -67,7 +72,7 @@ FOR image_and_details_to_import IN
       insert into indicia.taxon_media(taxon_meaning_id,path,caption,created_on,created_by_id,updated_on,updated_by_id,external_details,media_type_id,exif)
       values (
           (select taxon_meaning_id from indicia.cache_taxa_taxon_lists where taxon_list_id = <taxon_list_id> and taxon = image_and_details_to_import.taxRef_gattung || ' ' || image_and_details_to_import.art),
-          'comp_' || image_and_details_to_import.bildnummer || '.jpg',
+          '<filename_prefix>' || image_and_details_to_import.bildnummer || '.jpg',
           -- Caption must be 100 characters max 
           CASE WHEN 
             length(
@@ -127,32 +132,32 @@ FOR image_and_details_to_import IN
             END
           ),
           123,
-          cast(
-             '{"bildnummer":"' || coalesce(image_and_details_to_import.bildnummer, '')  || '", ' || 
-             '"taxRef_gattung":"' || coalesce(image_and_details_to_import.taxRef_gattung, '') || '", ' || 
-             '"art":"' || coalesce(image_and_details_to_import.art, '') || '", ' || 
-             '"taxref_ID":"' || coalesce(image_and_details_to_import.taxref_ID , '')|| '", ' || 
-             '"bildkategorie":"' || coalesce(image_and_details_to_import.bildkategorie, '') || '", ' || 
-             '"TKnr":"' || coalesce(image_and_details_to_import.TKnr, '') || '", ' || 
-             '"TKname":"' || coalesce(image_and_details_to_import.TKname, '') || '", ' || 
-             '"land":"' || coalesce(image_and_details_to_import.land, '') || '", ' || 
-             '"bundesland":"' || coalesce(image_and_details_to_import.bundesland, '') || '", ' || 
-             '"regierungsbezirk":"' || coalesce(image_and_details_to_import.regierungsbezirk, '') || '", ' || 
-             '"landkreis":"'  || coalesce(image_and_details_to_import.landkreis, '') || '", ' || 
-             '"fundort_1":"' || coalesce(image_and_details_to_import.fundort_1, '') || '", ' || 
-             '"NN_hohe":"' || coalesce(image_and_details_to_import.NN_hohe, '') || '", ' || 
-             '"koordinaten_1":"' || coalesce(image_and_details_to_import.koordinaten_1, '') || '", ' || 
-             '"koordinaten_2":"' || coalesce(image_and_details_to_import.koordinaten_2, '') || '", ' || 
-             '"begleitpflanzen":"' || coalesce(image_and_details_to_import.begleitpflanzen, '') || '", ' || 
-             '"datum_gesammelt":"' || coalesce(image_and_details_to_import.datum_gesammelt, '') || '", ' || 
-             '"leg":"' || coalesce(image_and_details_to_import.leg, '') || '", ' || 
-             '"det":"' || coalesce(image_and_details_to_import.det, '') || '", ' || 
-             '"conf":"' || coalesce(image_and_details_to_import.conf, '') || '", ' || 
-             '"fot":"' || coalesce(image_and_details_to_import.fot, '') || '", ' || 
-             '"herbar":"' || coalesce(image_and_details_to_import.herbar, '') || '", ' || 
-             '"herbarbelegnr":"' || coalesce(image_and_details_to_import.herbarbelegnr, '') || '", ' || 
-             '"anmerkung":"' || coalesce(image_and_details_to_import.anmerkung, '') || 
-             '"}' as text)::jsonb
+          json_build_object(
+            'bildnummer', image_and_details_to_import.bildnummer,
+            'taxRef_gattung', image_and_details_to_import.taxRef_gattung,
+            'art', image_and_details_to_import.art,
+            'taxref_ID', image_and_details_to_import.taxref_ID,
+            'bildkategorie', image_and_details_to_import.bildkategorie,
+            'TKnr', image_and_details_to_import.TKnr,
+            'TKname', image_and_details_to_import.TKname,
+            'land', image_and_details_to_import.land,
+            'bundesland', image_and_details_to_import.bundesland,
+            'regierungsbezirk', image_and_details_to_import.regierungsbezirk,
+            'landkreis', image_and_details_to_import.landkreis,
+            'fundort_1', image_and_details_to_import.fundort_1,
+            'NN_hohe', image_and_details_to_import.NN_hohe,
+            'koordinaten_1', image_and_details_to_import.koordinaten_1,
+            'koordinaten_2', image_and_details_to_import.koordinaten_2,
+            'begleitpflanzen', image_and_details_to_import.begleitpflanzen,
+            'datum_gesammelt', image_and_details_to_import.datum_gesammelt,
+            'leg', image_and_details_to_import.leg,
+            'det', image_and_details_to_import.det,
+            'conf', image_and_details_to_import.conf,
+            'fot', image_and_details_to_import.fot,
+            'herbar', image_and_details_to_import.herbar,
+            'herbarbelegnr', image_and_details_to_import.herbarbelegnr,
+            'anmerkung', image_and_details_to_import.anmerkung
+          )
       );
     ELSE
     END IF;
