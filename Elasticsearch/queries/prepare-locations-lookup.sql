c17827655750d5b87faa4a3459ffe2bd93c8f70a
John van Breda <john.vanbreda@biodiverseit.co.uk>
2020-02-28 12:25:24 +0000

    Sort the output for consistency
    Reduces size of changes in Git commits.

--- Elasticsearch/queries/prepare-locations-lookup.sql
+++ Elasticsearch/queries/prepare-locations-lookup.sql
@@ -3,4 +3,5 @@ FROM locations l
 JOIN cache_termlists_terms t ON t.id=l.location_type_id
 WHERE l.deleted=false
 AND l.location_type_id in (<indexed_location_type_ids>)
-AND COALESCE(l.code, '') NOT LIKE '%+%';
+AND COALESCE(l.code, '') NOT LIKE '%+%'
+ORDER BY t.term, l.name;
