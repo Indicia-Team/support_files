077472c8eb6afeaff2703b78e2aee46c90f56bbb
Andrew van Breda <support@avb-it.co.uk>
2020-07-04 23:49:48 +0100

    Descriptions can now also be marked as complete

--- DGfM/Custom site files/Node js/node.36.js
+++ DGfM/Custom site files/Node js/node.36.js
@@ -1,5 +1,23 @@
+var mark_as_complete;
 var reject_completion;
 jQuery(document).ready(function($) {
+  mark_as_complete = function(cttl_id) {
+    var r = confirm('Are you sure you want to complete the description of this fungus?');
+    if (r == true) {
+      var postUrl='http://www.fungi-without-borders.eu/en/iform/ajaxproxy?node=&index=taxa_taxon_list_attribute_value';
+      $.post(postUrl,
+        {"website_id":"2","taxa_taxon_list_attribute_id":2197, "taxa_taxon_list_id":cttl_id, "int_value":1},
+        function (data) {
+          location.reload();
+        },
+        'json'
+      );
+      
+    } else {
+      return false;
+    }
+  }
+
   reject_completion = function(ttlav_id) {
     var r = confirm('Are you sure you want to undo the completion of this fungus?');
     if (r == true) {
