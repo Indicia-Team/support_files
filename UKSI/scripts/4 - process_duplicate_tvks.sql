d32372ae3973ecdbed82bcc9201cc37f1e5b4f8c
John van Breda <john.vanbreda@biodiverseit.co.uk>
2021-01-12 11:56:52 +0000

    Duplicate TVK cleanup now deletes
    Leaves DB clean rather than just moves occurrences over.

--- UKSI/scripts/4 - process_duplicate_tvks.sql
+++ UKSI/scripts/4 - process_duplicate_tvks.sql
@@ -1,6 +1,6 @@
 
 
-SELECT d1.taxa_taxon_list_id AS from_taxa_taxon_list_id, d2.taxa_taxon_list_id AS to_taxa_taxon_list_id
+SELECT d1.taxa_taxon_list_id AS from_taxa_taxon_list_id, d1.taxon_id as from_taxon_id, d2.taxa_taxon_list_id AS to_taxa_taxon_list_id
 INTO TEMPORARY ttl_changes
 FROM uksi.duplicates d1
 JOIN uksi.duplicates d2 ON d2.search_code=d1.search_code
@@ -26,6 +26,24 @@ SET taxa_taxon_list_id=tc.to_taxa_taxon_list_id,
 	updated_by_id=(select updated_by_user_id from uksi.uksi_settings)
 FROM ttl_changes tc
 WHERE tc.from_taxa_taxon_list_id = d.taxa_taxon_list_id;
+
+-- Clean up the duplicates.
+UPDATE taxa_taxon_lists ttl
+SET deleted=true, 
+  updated_on=now(), 
+  updated_by_id=(select updated_by_user_id from uksi.uksi_settings)
+FROM ttl_changes tc
+WHERE tc.from_taxa_taxon_list_id = ttl.id;
+
+UPDATE taxa t
+SET deleted=true, 
+  updated_on=now(), 
+  updated_by_id=(select updated_by_user_id from uksi.uksi_settings)
+FROM ttl_changes tc
+WHERE tc.from_taxon_id = t.id;
+
+DELETE FROM cache_taxa_taxon_lists WHERE id IN (SELECT from_taxa_taxon_list_id FROM ttl_changes);
+DELETE FROM cache_taxon_searchterms WHERE taxa_taxon_list_id IN (SELECT from_taxa_taxon_list_id FROM ttl_changes);
 
 DROP TABLE uksi.duplicates;
 DROP TABLE ttl_changes;
