f1ccbbc9842b0e99830c14021aba6ad82e846d1b
John van Breda <john.vanbreda@biodiverseit.co.uk>
2021-01-13 11:56:58 +0000

    Allow data entry check fix

--- UKSI/scripts/24 - match to existing taxa taxon lists.sql
+++ UKSI/scripts/24 - match to existing taxa taxon lists.sql
@@ -15,7 +15,7 @@ SET id=ttl.id,
   orig_taxon_meaning_id=ttl.taxon_meaning_id,
   orig_parent_id=ttl.parent_id,
   orig_common_taxon_id=ttl.common_taxon_id,
-  changed=changed OR allow_data_entry=false
+  changed=changed OR ttl.allow_data_entry<>pttl.allow_data_entry
 FROM taxa t
 JOIN taxa_taxon_lists ttl
   ON ttl.taxon_id=t.id
