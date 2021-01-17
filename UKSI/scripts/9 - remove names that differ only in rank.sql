522cb9cffa77583d5be87860280190ebbe1a6499
John van Breda <john.vanbreda@biodiverseit.co.uk>
2021-01-17 15:57:24 +0000

    All names fixes
    * Only mark as not for data entry - in case UKSI operation promotes a name.
    * If organism master has 2 copies of a name (TVK) but one is redundant, that copy can be deleted.

--- UKSI/scripts/9 - remove names that differ only in rank.sql
+++ UKSI/scripts/9 - remove names that differ only in rank.sql
@@ -1,6 +1,6 @@
 SET search_path=uksi, public;
 
-DELETE FROM all_names WHERE input_taxon_version_key IN (
+UPDATE all_names SET redundant=true WHERE input_taxon_version_key IN (
   -- find duplicate names to delete which differ only in rank and are a different rank to the preferred name
   SELECT DISTINCT l1.input_taxon_version_key
   FROM all_names l1
@@ -23,3 +23,11 @@ DELETE FROM all_names WHERE input_taxon_version_key IN (
   WHERE l1.recommended_taxon_version_key<>l1.input_taxon_version_key
   AND l1.redundant=false
 );
+
+-- Delete redundant copies of names where there is a second non-redundant
+-- copy (due to duplication in ORGANISM_MASTER).
+DELETE from uksi.all_names WHERE input_taxon_version_key IN (
+  SELECT an1.input_taxon_version_key FROM uksi.all_names an1
+  JOIN uksi.all_names an2 ON an2.input_taxon_version_key=an1.input_taxon_version_key
+  WHERE an1.redundant=true AND an2.redundant=false
+) AND redundant=true;
