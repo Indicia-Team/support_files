e1dc873d799e9760a8c29119fabdf46247d68fdf
Andrew van Breda <support@avb-it.co.uk>
2021-03-17 12:01:49 +0000

    Remove some duplicates and re-commit other files after restructuring of the Plant Portal project folder in support_files

--- Plant Portal Importer/Node JS Files/node.11.js
+++
@@ -1,7 +0,0 @@
-// Change references to Group to say Project in the importer, and remove fields that aren't needed.
-jQuery(window).load(function () {
-  jQuery('[label="Groups location"]').prop('label','Projects');
-  jQuery('[value="groups_location:fk_group"]').prop('label','Project (from controlled termlist)');
-  jQuery('[value="groups_location\\:fk_created_by"]').remove();
-  jQuery('[value="groups_location\\:id"]').remove();
-});
