5096151794b9807cdaa4770472f4f9d6fd3d8f11
Andrew van Breda <support@avb-it.co.uk>
2021-03-25 06:41:33 +0000

    No no functionality, but as pages split into My Project and All Projects pages, another JS file is required.

---
+++ Plant Portal/Node JS files/node.34.js
@@ -0,0 +1,7 @@
+jQuery(document).ready(function () {
+  // Hide the links to the data entry pages as these must be associated with a group, 
+  // but are actually accessed via another page
+  jQuery('.node-wildflower-data-entry').hide();
+  jQuery('.node-indicator-data-entry').hide();
+  jQuery('.node-inventory-data-entry').hide();
+});
