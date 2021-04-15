a9791316e2125c79f541db2901601126801ac168
Andrew van Breda <support@avb-it.co.uk>
2021-04-15 15:05:02 +0100

    Latest code that sets up the correct links for each type of Plant Portal project.

--- Plant Portal/Node JS files/node.13.js
+++ Plant Portal/Node JS files/node.13.js
@@ -22,23 +22,23 @@ jQuery(document).ready(function () {
     	// Put data entry selection page onto existing rows
       	jQuery('[name="group+\\:pages\\:\\:0\\:0"] option[value="node/25:NPMS Mode - data entry level selection"]').prop('selected', true);  
       	jQuery('[name="group+\\:pages\\:\\:0\\:1"]').val('Enter data'); 
-      	// Put user plot list onto existing row
-      	jQuery('[name="group+\\:pages\\:\\:1\\:0"] option[value="my-plots-npms-mode:NPMS Mode - my plots"]').prop('selected', true);
-      	jQuery('[name="group+\\:pages\\:\\:1\\:1"]').val('My plots'); 
+      	// Put user square list onto existing row
+      	jQuery('[name="group+\\:pages\\:\\:1\\:0"] option[value="list-squares-npms-mode:NPMS Mode - List squares"]').prop('selected', true);
+      	jQuery('[name="group+\\:pages\\:\\:1\\:1"]').val('List squares');
 	    // Put admin plot management onto existing row
-      	jQuery('[name="group+\\:pages\\:\\:2\\:0"] option[value="project-plot-list-npms-mode:NPMS Mode - project plot list"]').prop('selected', true);
-      	jQuery('[name="group+\\:pages\\:\\:2\\:1"]').val('Manage project plots'); 
-      	jQuery('[name="group+\\:pages\\:\\:2\\:2"] option[value="t:Available only to group admins"]').prop('selected', true);
+      	//jQuery('[name="group+\\:pages\\:\\:2\\:0"] option[value="project-plot-list-npms-mode:NPMS Mode - project plot list"]').prop('selected', true);
+      	//jQuery('[name="group+\\:pages\\:\\:2\\:1"]').val('Manage project plots'); 
+      	//jQuery('[name="group+\\:pages\\:\\:2\\:2"] option[value="t:Available only to group admins"]').prop('selected', true);
       	
       	// Add new rows to allow us to setup Wildflower/Indicator/Inventory
-      	jQuery('#complex-attr-grid-group-pages').append('<input type="text" name="group+:pages::3:0" value="node/wildflower-data-entry:NPMS Mode - Wildflower">');
-	  	jQuery('#complex-attr-grid-group-pages').append('<input type="text" name="group+:pages::3:1" value="Wildflower">');
+      	jQuery('#complex-attr-grid-group-pages').append('<input type="text" name="group+:pages::2:0" value="node/wildflower-data-entry:NPMS Mode - Wildflower">');
+	  	jQuery('#complex-attr-grid-group-pages').append('<input type="text" name="group+:pages::2:1" value="Wildflower">');
       	
-	  	jQuery('#complex-attr-grid-group-pages').append('<input type="text" name="group+:pages::4:0" value="node/indicator-data-entry:NPMS Mode - Indicator">');
-	  	jQuery('#complex-attr-grid-group-pages').append('<input type="text" name="group+:pages::4:1" value="Indicator">');
+	  	jQuery('#complex-attr-grid-group-pages').append('<input type="text" name="group+:pages::3:0" value="node/indicator-data-entry:NPMS Mode - Indicator">');
+	  	jQuery('#complex-attr-grid-group-pages').append('<input type="text" name="group+:pages::3:1" value="Indicator">');
 	  
-	  	jQuery('#complex-attr-grid-group-pages').append('<input type="text" name="group+:pages::5:0" value="node/inventory-data-entry:NPMS Mode - Inventory">');
-	  	jQuery('#complex-attr-grid-group-pages').append('<input type="text" name="group+:pages::5:1" value="Inventory">');
+	  	jQuery('#complex-attr-grid-group-pages').append('<input type="text" name="group+:pages::4:0" value="node/inventory-data-entry:NPMS Mode - Inventory">');
+	  	jQuery('#complex-attr-grid-group-pages').append('<input type="text" name="group+:pages::4:1" value="Inventory">');
 
       }
     
@@ -49,25 +49,25 @@ jQuery(document).ready(function () {
       	jQuery('[name="group+\\:pages\\:\\:0\\:1"]').val('Enter data'); 
       	
       	// Put user plot list onto existing row
-      	jQuery('[name="group+\\:pages\\:\\:1\\:0"] option[value="my-plots-standard-mode:Standard Mode - my plots"]').prop('selected', true);
-      	jQuery('[name="group+\\:pages\\:\\:1\\:1"]').val('My plots'); 
+      	jQuery('[name="group+\\:pages\\:\\:1\\:0"] option[value="list-plots-standard-mode:Standard Mode - List plots"]').prop('selected', true);
+      	jQuery('[name="group+\\:pages\\:\\:1\\:1"]').val('List plots'); 
 
-      	jQuery('[name="group+\\:pages\\:\\:2\\:0"] option[value="project-plot-list-standard-mode:Standard Mode - project plot list"]').prop('selected', true);
-      	jQuery('[name="group+\\:pages\\:\\:2\\:1"]').val('Manage project plots'); 
-      	jQuery('[name="group+\\:pages\\:\\:2\\:2"] option[value="t:Available only to group admins"]').prop('selected', true);
+      	//jQuery('[name="group+\\:pages\\:\\:2\\:0"] option[value="project-plot-list-standard-mode:Standard Mode - project plot list"]').prop('selected', true);
+      	//jQuery('[name="group+\\:pages\\:\\:2\\:1"]').val('Manage project plots'); 
+      	//jQuery('[name="group+\\:pages\\:\\:2\\:2"] option[value="t:Available only to group admins"]').prop('selected', true);
       	
       	// We don't need the additional rows used for all the NPMS forms in Standard Mode
+      	jQuery('[name="group+:pages::2:0"]').remove(); 
+      	jQuery('[name="group+:pages::2:1"]').remove();
+      	jQuery('[name="group+:pages::2:2"]').remove();  
+      	
       	jQuery('[name="group+:pages::3:0"]').remove(); 
       	jQuery('[name="group+:pages::3:1"]').remove();
-      	jQuery('[name="group+:pages::3:2"]').remove();  
+      	jQuery('[name="group+:pages::3:2"]').remove();
       	
       	jQuery('[name="group+:pages::4:0"]').remove(); 
       	jQuery('[name="group+:pages::4:1"]').remove();
       	jQuery('[name="group+:pages::4:2"]').remove();
-      	
-      	jQuery('[name="group+:pages::5:0"]').remove(); 
-      	jQuery('[name="group+:pages::5:1"]').remove();
-      	jQuery('[name="group+:pages::5:2"]').remove();
 
       }
   	});
