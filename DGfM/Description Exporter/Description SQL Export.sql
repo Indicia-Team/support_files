fbcd1c39fec1755cb7dbaa8c11b92b3882dbd320
Andrew van Breda <support@avb-it.co.uk>
2021-06-03 16:54:38 +0100

    Lots of changes to how this works.

--- DGfM/Description Exporter/Description SQL Export.sql
+++ DGfM/Description Exporter/Description SQL Export.sql
@@ -3,22 +3,29 @@ DROP table complete_descriptions;
 -- Create a table which holds the taxa_taxon_list_id and preferred species name
 CREATE TABLE complete_descriptions ("id" INT, "name" VARCHAR);
 
--- Add the attributes to the table as columns. As there is a limit to the number of columns, replace XXX to limit attributes added to table
+-- Add the attributes to the table as columns. As there is a limit to the number of columns allowed, rerun this firstly with Limit 1000 & Offset 0
+-- then rerun but put everything into a new table complete_descriptions_2 with Limit 1500 & Offset 1000 for remaining attributes
+-- Note: these numbers may need to be altered in the future if the numbers of attributes increases.
 -- Also as columns have limit to column characters, we take first 15 characters of area, then sub area, and caption, then add attribute ID on end (and replace all spaces with underscores)
 DO
 $do$
-    DECLARE colnames TEXT;
+    DECLARE attribute_record RECORD;
 BEGIN
-FOR colnames IN 
-    SELECT lower(regexp_replace(REPLACE(LEFT(ctt_area.term,15) || ' ' || LEFT(ctt_sub_area.term,15) || ' ' || LEFT(ttla.caption,15) || ' ' || ttla.id, ' ', '_'), '\W+', '', 'g'))
+FOR attribute_record IN 
+    SELECT lower(regexp_replace(REPLACE(LEFT(ctt_area.term,15) || ' ' || LEFT(ctt_sub_area.term,15) || ' ' || LEFT(ttla.caption,15) || ' ' || ttla.id, ' ', '_'), '\W+', '', 'g')) as col_name
     FROM indicia.taxa_taxon_list_attributes ttla
     JOIN indicia.taxon_lists_taxa_taxon_list_attributes tlttla on tlttla.taxa_taxon_list_attribute_id=ttla.id and tlttla.taxon_list_id = 1 and tlttla.deleted=false and tlttla.id not in (30,33,2134,2132,2133)
     JOIN indicia.cache_termlists_terms ctt_sub_area on ctt_sub_area.id = ttla.reporting_category_id 
     JOIN indicia.cache_termlists_terms ctt_area on ctt_area.id = ctt_sub_area.parent_id
-    WHERE ttla.deleted=false AND ttla.id > XXX AND ttla.id < XXX
+    WHERE ttla.deleted=false
+    GROUP BY ctt_area.term, ctt_sub_area.term, ttla.id, ttla.caption,tlttla.weight
     ORDER BY ctt_area.term,ctt_sub_area.term,tlttla.weight asc
+    Limit 1000
+    Offset 0
+    --Limit 1500
+    --Offset 1000
 LOOP
-    EXECUTE 'ALTER TABLE complete_descriptions ADD COLUMN ' || quote_ident(colnames) || ' VARCHAR DEFAULT NULL;'; /* careful: in quoted text, the spaces are important */
+    EXECUTE 'ALTER TABLE complete_descriptions ADD COLUMN ' || attribute_record.col_name/*quote_ident(colnames)*/ || ' VARCHAR DEFAULT NULL;'; /* careful: in quoted text, the spaces are important */
 END LOOP;
 END
 $do$;
@@ -46,7 +53,7 @@ $do$;
 DO
 $do$
     DECLARE taxon_record_2 RECORD;
-    DECLARE attribute_record RECORD;
+    DECLARE attribute_record_2 RECORD;
     DECLARE stmt2 varchar;
     DECLARE colToUse varchar;
 BEGIN
@@ -55,7 +62,7 @@ FOR taxon_record_2 IN
     FROM complete_descriptions cd_table
     ORDER BY cd_table.name asc
 LOOP
-FOR attribute_record IN 
+FOR attribute_record_2 IN 
     SELECT ttla.id,ttla.caption,
     string_agg(distinct coalesce(
         case when ttla.data_type = 'L' then coalesce(ctt_german.term,ctt_english.term) else null end,
@@ -78,7 +85,6 @@ FOR attribute_record IN
     LEFT JOIN indicia.cache_termlists_terms ctt_czech on ctt_czech.meaning_id = ctt_meaning.meaning_id  AND ctt_czech.language_iso='cze'  
 
     JOIN indicia.taxa_taxon_list_attributes ttla on ttla.id = ttlav.taxa_taxon_list_attribute_id AND ttla.deleted=false 
-        AND ttla.id > XXX AND ttla.id < XXX
     JOIN indicia.taxon_lists_taxa_taxon_list_attributes tlttla on tlttla.taxa_taxon_list_attribute_id=ttla.id and tlttla.taxon_list_id = 1 and tlttla.deleted=false and tlttla.id not in (30,33,2134,2132,2133)
     JOIN indicia.cache_termlists_terms ctt_sub_area on ctt_sub_area.id = ttla.reporting_category_id 
     JOIN indicia.cache_termlists_terms ctt_area on ctt_area.id = ctt_sub_area.parent_id
@@ -87,8 +93,18 @@ FOR attribute_record IN
     GROUP BY ctt_area.term, ctt_sub_area.term, ttla.id, ttla.caption,tlttla.weight
     ORDER BY ctt_area.term,ctt_sub_area.term,tlttla.weight asc
 LOOP
-colToUse = lower(regexp_replace(REPLACE(LEFT(attribute_record.area,15) || ' ' || LEFT(attribute_record.sub_area,15) || ' ' || LEFT(attribute_record.caption,15) || ' ' || attribute_record.id, ' ', '_'), '\W+', '', 'g'));
-stmt2 = 'update complete_descriptions set ' || colToUse || ' = ''' || REPLACE(attribute_record.attribute_value,'''','') || ''' where id = ' || taxon_record_2.cycling_taxa_taxon_list_id;
+colToUse = lower(regexp_replace(REPLACE(LEFT(attribute_record_2.area,15) || ' ' || LEFT(attribute_record_2.sub_area,15) || ' ' || LEFT(attribute_record_2.caption,15) || ' ' || attribute_record_2.id, ' ', '_'), '\W+', '', 'g'));
+stmt2 = '
+DO $$   
+BEGIN               
+if exists(
+select 1 from information_schema.columns
+where table_schema=''public'' and table_name=''complete_descriptions'' and column_name = ''' || colToUse || ''')  
+THEN
+update complete_descriptions set ' || colToUse || ' = ''' || REPLACE(attribute_record_2.attribute_value,'''','') || ''' where id = ' || taxon_record_2.cycling_taxa_taxon_list_id || '; 
+END IF;
+END
+$$;';
 EXECUTE stmt2;
 END LOOP;
 END LOOP;
