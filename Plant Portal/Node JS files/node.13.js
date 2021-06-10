9d2113cf9ee07f750e044a5ff4de85b41e9a279b
Andrew van Breda <support@avb-it.co.uk>
2021-06-10 13:14:45 +0100

    - Improved instructions - Remove unneeded code - Improved elegance - Slight alterations needed as pages names have altered.

--- Plant Portal/Node JS files/node.13.js
+++ Plant Portal/Node JS files/node.13.js
@@ -1,3 +1,4 @@
+// Note there are IDs in this script which are setup for the BRC Dev Warehouse. 
 jQuery(document).ready(function () {
   //Code to automatically select required page links when user selects project type.
   
@@ -8,27 +9,37 @@ jQuery(document).ready(function () {
   const urlParamsString = window.location.search;
   const urlParams = new URLSearchParams(urlParamsString);
   
+  // Always make sure we start with 2 rows
+  function resets_rows() {
+    jQuery('[name="group+:pages::2:0"]').remove(); 
+    jQuery('[name="group+:pages::2:1"]').remove();
+    jQuery('[name="group+:pages::2:2"]').remove();  
+      	
+    jQuery('[name="group+:pages::3:0"]').remove(); 
+    jQuery('[name="group+:pages::3:1"]').remove();
+    jQuery('[name="group+:pages::3:2"]').remove();
+      	
+    jQuery('[name="group+:pages::4:0"]').remove(); 
+    jQuery('[name="group+:pages::4:1"]').remove();
+    jQuery('[name="group+:pages::4:2"]').remove();
+  }
+  
   if (!urlParams.get('group_id')) {
   	// Setup links as mode is selected
   	jQuery('#group\\:group_type_id').on('change', function() {
   	  //NPMS Mode
       if (jQuery(this).val() == 15068) {
-      
-        // We need the third row again if it was hidden by Standard Mode
-      	jQuery('[name="group+\\:pages\\:\\:2\\:0"]').show();
-      	jQuery('[name="group+\\:pages\\:\\:2\\:1"]').show();
-	  	jQuery('[name="group+\\:pages\\:\\:2\\:2"]').show();
-	  	jQuery('[name="group+\\:pages\\:\\:2\\:deleted"]').show();
+      // Current version for Live Warehouse
+      //if (jQuery(this).val() == 18067) {  
+        // Always make sure we start with 2 rows
+		resets_rows();
+
     	// Put data entry selection page onto existing rows
-      	jQuery('[name="group+\\:pages\\:\\:0\\:0"] option[value="node/25:NPMS Mode - data entry level selection"]').prop('selected', true);  
+      	jQuery('[name="group+\\:pages\\:\\:0\\:0"] option[value="npms-mode-data-entry-selection:NPMS Mode - data entry level selection"]').prop('selected', true);  
       	jQuery('[name="group+\\:pages\\:\\:0\\:1"]').val('Enter data'); 
       	// Put user square list onto existing row
       	jQuery('[name="group+\\:pages\\:\\:1\\:0"] option[value="list-squares-npms-mode:NPMS Mode - List squares"]').prop('selected', true);
       	jQuery('[name="group+\\:pages\\:\\:1\\:1"]').val('List squares');
-	    // Put admin plot management onto existing row
-      	//jQuery('[name="group+\\:pages\\:\\:2\\:0"] option[value="project-plot-list-npms-mode:NPMS Mode - project plot list"]').prop('selected', true);
-      	//jQuery('[name="group+\\:pages\\:\\:2\\:1"]').val('Manage project plots'); 
-      	//jQuery('[name="group+\\:pages\\:\\:2\\:2"] option[value="t:Available only to group admins"]').prop('selected', true);
       	
       	// Add new rows to allow us to setup Wildflower/Indicator/Inventory
       	jQuery('#complex-attr-grid-group-pages').append('<input type="text" name="group+:pages::2:0" value="node/wildflower-data-entry:NPMS Mode - Wildflower">');
@@ -44,6 +55,10 @@ jQuery(document).ready(function () {
     
       //Standard Mode
       if (jQuery(this).val() == 15069) {
+      // Current version for Live Warehouse
+      //if (jQuery(this).val() == 18068) {  
+        // Always make sure we start with 2 rows
+		resets_rows();
     	
       	jQuery('[name="group+\\:pages\\:\\:0\\:0"] option[value="standard-mode-data-entry:Standard Mode - data entry"]').prop('selected', true);  
       	jQuery('[name="group+\\:pages\\:\\:0\\:1"]').val('Enter data'); 
@@ -52,22 +67,6 @@ jQuery(document).ready(function () {
       	jQuery('[name="group+\\:pages\\:\\:1\\:0"] option[value="list-plots-standard-mode:Standard Mode - List plots"]').prop('selected', true);
       	jQuery('[name="group+\\:pages\\:\\:1\\:1"]').val('List plots'); 
 
-      	//jQuery('[name="group+\\:pages\\:\\:2\\:0"] option[value="project-plot-list-standard-mode:Standard Mode - project plot list"]').prop('selected', true);
-      	//jQuery('[name="group+\\:pages\\:\\:2\\:1"]').val('Manage project plots'); 
-      	//jQuery('[name="group+\\:pages\\:\\:2\\:2"] option[value="t:Available only to group admins"]').prop('selected', true);
-      	
-      	// We don't need the additional rows used for all the NPMS forms in Standard Mode
-      	jQuery('[name="group+:pages::2:0"]').remove(); 
-      	jQuery('[name="group+:pages::2:1"]').remove();
-      	jQuery('[name="group+:pages::2:2"]').remove();  
-      	
-      	jQuery('[name="group+:pages::3:0"]').remove(); 
-      	jQuery('[name="group+:pages::3:1"]').remove();
-      	jQuery('[name="group+:pages::3:2"]').remove();
-      	
-      	jQuery('[name="group+:pages::4:0"]').remove(); 
-      	jQuery('[name="group+:pages::4:1"]').remove();
-      	jQuery('[name="group+:pages::4:2"]').remove();
 
       }
   	});
