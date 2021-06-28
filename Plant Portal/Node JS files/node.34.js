a2fa482772cd45750feef9295cb1dfc88240ed29
Andrew van Breda <support@avb-it.co.uk>
2021-06-28 22:13:45 +0100

    Supports newer and older test projects which use older link names

--- Plant Portal/Node JS files/node.34.js
+++ Plant Portal/Node JS files/node.34.js
@@ -1,9 +1,17 @@
 jQuery(document).ready(function () {
   // Hide the links to the data entry pages as these must be associated with a group, 
   // but are actually accessed via another page
+  jQuery('.wildflower-data-entry').hide();
+  jQuery('.indicator-data-entry').hide();
+  jQuery('.inventory-data-entry').hide();
+  
+  // Remove link types for old projects. 
+  // This has changed, so this bit of code should not be needed going forward once test 
+  // projects are removed from system.
   jQuery('.node-wildflower-data-entry').hide();
   jQuery('.node-indicator-data-entry').hide();
   jQuery('.node-inventory-data-entry').hide();
+  
   // Make sure the links aren't all on one line
-  jQuery('<br>').insertBefore('.node-inventory-data-entry');
+  jQuery('<br>').insertBefore('.npms-mode-data-entry-selection');
 });
