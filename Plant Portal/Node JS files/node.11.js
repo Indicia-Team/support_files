f0f1d713da583009c47c8888bfc83c3b1081c95d
Andrew van Breda <support@avb-it.co.uk>
2021-07-01 12:13:13 +0100

    Show and hide appropriate fields in NPMS Mode squares importer

--- Plant Portal/Node JS files/node.11.js
+++ Plant Portal/Node JS files/node.11.js
@@ -1,7 +1,29 @@
-// Change references to Group to say Project in the importer, and remove fields that aren't needed.
-jQuery(window).load(function () {
-  jQuery('[label="Groups location"]').prop('label','Projects');
+// Change the labels inside the importer as required.
+jQuery(document).ready(function () {
+  // Change the labels on the option sections in the select lists
+  jQuery('[label="Groups location"]').prop('label','Project details');
+  jQuery('[label="Location"]').prop('label','Square details');
+  // Change the select option labels as required
   jQuery('[value="groups_location:fk_group"]').prop('label','Project (from controlled termlist)');
-  jQuery('[value="groups_location\\:fk_created_by"]').remove();
-  jQuery('[value="groups_location\\:id"]').remove();
+  jQuery('[value="location:name"]').prop('label','Square name');
+  jQuery('[value="location:centroid_sref"]').prop('label','Spatial reference');
+  jQuery('[value="location:centroid_sref_system"]').prop('label','Spatial reference system');
+
+  // Remove all options apart from the ones we need
+  jQuery('.import-mappings-table').find('option').each(function() {
+    if (jQuery(this).val() !== 'groups_location:fk_group' &&
+        jQuery(this).val() !== 'location:name' &&
+        jQuery(this).val() !== 'location:centroid_sref' &&
+        jQuery(this).val() !== 'location:centroid_sref_system') {
+      jQuery(this).remove();
+    }
+  });
+
+  // Clear the drop-down section labels we aren't going to use
+  jQuery('select').find('optgroup').each(function() {
+    if (jQuery(this).prop('label') !== 'Project details' &&
+        jQuery(this).prop('label') !== 'Square details') {
+      jQuery(this).prop('label','');
+    }
+  });
 });
