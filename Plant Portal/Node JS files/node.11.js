6cc94dd8b02a2af21601a05b626051ac61c7266e
Andrew van Breda <support@avb-it.co.uk>
2021-07-05 16:59:40 +0100

    Channge all the committed node .js to be the ones from Live rather than Dev.

--- Plant Portal/Node JS files/node.11.js
+++ Plant Portal/Node JS files/node.11.js
@@ -1,29 +1,18 @@
-// Change the labels inside the importer as required.
 jQuery(document).ready(function () {
-  // Change the labels on the option sections in the select lists
-  jQuery('[label="Groups location"]').prop('label','Project details');
-  jQuery('[label="Location"]').prop('label','Square details');
-  // Change the select option labels as required
-  jQuery('[value="groups_location:fk_group"]').prop('label','Project (from controlled termlist)');
-  jQuery('[value="location:name"]').prop('label','Square name');
-  jQuery('[value="location:centroid_sref"]').prop('label','Spatial reference');
-  jQuery('[value="location:centroid_sref_system"]').prop('label','Spatial reference system');
-
-  // Remove all options apart from the ones we need
-  jQuery('.import-mappings-table').find('option').each(function() {
-    if (jQuery(this).val() !== 'groups_location:fk_group' &&
-        jQuery(this).val() !== 'location:name' &&
-        jQuery(this).val() !== 'location:centroid_sref' &&
-        jQuery(this).val() !== 'location:centroid_sref_system') {
-      jQuery(this).remove();
-    }
-  });
-
-  // Clear the drop-down section labels we aren't going to use
-  jQuery('select').find('optgroup').each(function() {
-    if (jQuery(this).prop('label') !== 'Project details' &&
-        jQuery(this).prop('label') !== 'Square details') {
-      jQuery(this).prop('label','');
-    }
-  });
+  // Hide the links to the data entry pages as these must be associated with a group, 
+  // but are actually accessed via another page
+  jQuery('.wildflower-data-entry').hide();
+  jQuery('.indicator-data-entry').hide();
+  jQuery('.inventory-data-entry').hide();
+  
+  // Remove link types for old projects. 
+  // This has changed, so this bit of code should not be needed going forward once test 
+  // projects are removed from system.
+  jQuery('.node-wildflower-data-entry').hide();
+  jQuery('.node-indicator-data-entry').hide();
+  jQuery('.node-inventory-data-entry').hide();
+  
+  // Make sure the links aren't all on one line
+  jQuery('<br>').insertBefore('.my-samples-npms-mode');
+  jQuery('<br>').insertBefore('.npms-mode-data-entry-selection');
 });
