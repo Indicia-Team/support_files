d4d0161b7e3628e16e8e24275c5e1b3e8cb344d8
Andrew van Breda <support@avb-it.co.uk>
2021-07-06 20:49:39 +0100

    Fixed problem where the List Squares list was showing for users who hadn't joined a group yet.

--- Plant Portal/Node JS files/node.30.js
+++ Plant Portal/Node JS files/node.30.js
@@ -40,11 +40,13 @@ jQuery(document).ready(function () {
   	// Setup links as mode is selected
   	jQuery('#group\\:group_type_id').on('change', function() {
   	  //NPMS Mode
-      if (jQuery(this).val() == 18067) { 
+      if (jQuery(this).val() == 18067) {
+      // Current version for Live Warehouse
+      //if (jQuery(this).val() == 18067) {  
         // Always make sure we start with 2 rows
-		    resets_rows();
+		resets_rows();
 
-    	  // Put data entry selection page onto existing rows
+    	// Put data entry selection page onto existing rows
       	jQuery('[name="group+\\:pages\\:\\:0\\:0"] option[value="npms-mode-data-entry-selection:NPMS Mode - data entry level selection"]').prop('selected', true);  
       	jQuery('[name="group+\\:pages\\:\\:0\\:1"]').val('Enter data'); 
       	
@@ -54,24 +56,28 @@ jQuery(document).ready(function () {
       	
       	// Add new rows to allow us to see the squares for the project
       	jQuery('#complex-attr-grid-group-pages').append('<input type="text" name="group+:pages::2:0" value="list-squares-npms-mode:NPMS Mode - List squares">');
-	  	  jQuery('#complex-attr-grid-group-pages').append('<input type="text" name="group+:pages::2:1" value="List squares">');
+	  	jQuery('#complex-attr-grid-group-pages').append('<input type="text" name="group+:pages::2:1" value="List squares">');
+	  	jQuery('#complex-attr-grid-group-pages').append('<input type="text" name="group+:pages::2:2" value="f:Available only to group members">');
+
       	
       	// Add new rows to allow us to setup Wildflower/Indicator/Inventory
       	jQuery('#complex-attr-grid-group-pages').append('<input type="text" name="group+:pages::3:0" value="wildflower-data-entry:NPMS Mode - Wildflower">');
-	  	  jQuery('#complex-attr-grid-group-pages').append('<input type="text" name="group+:pages::3:1" value="Wildflower">');
+	  	jQuery('#complex-attr-grid-group-pages').append('<input type="text" name="group+:pages::3:1" value="Wildflower">');
       	
-	  	  jQuery('#complex-attr-grid-group-pages').append('<input type="text" name="group+:pages::4:0" value="indicator-data-entry:NPMS Mode - Indicator">');
-	  	  jQuery('#complex-attr-grid-group-pages').append('<input type="text" name="group+:pages::4:1" value="Indicator">');
+	  	jQuery('#complex-attr-grid-group-pages').append('<input type="text" name="group+:pages::4:0" value="indicator-data-entry:NPMS Mode - Indicator">');
+	  	jQuery('#complex-attr-grid-group-pages').append('<input type="text" name="group+:pages::4:1" value="Indicator">');
 	  
-	  	  jQuery('#complex-attr-grid-group-pages').append('<input type="text" name="group+:pages::5:0" value="inventory-data-entry:NPMS Mode - Inventory">');
-	  	j Query('#complex-attr-grid-group-pages').append('<input type="text" name="group+:pages::5:1" value="Inventory">');
+	  	jQuery('#complex-attr-grid-group-pages').append('<input type="text" name="group+:pages::5:0" value="inventory-data-entry:NPMS Mode - Inventory">');
+	  	jQuery('#complex-attr-grid-group-pages').append('<input type="text" name="group+:pages::5:1" value="Inventory">');
 
       }
     
       //Standard Mode
       if (jQuery(this).val() == 18068) {
+      // Current version for Live Warehouse
+      //if (jQuery(this).val() == 18068) {  
         // Always make sure we start with 2 rows
-		    resets_rows();
+		resets_rows();
     	
       	jQuery('[name="group+\\:pages\\:\\:0\\:0"] option[value="standard-mode-data-entry:Standard Mode - data entry"]').prop('selected', true);  
       	jQuery('[name="group+\\:pages\\:\\:0\\:1"]').val('Enter data'); 
