1e24f6a9f4f5168bbd034f9d10e6efc65e76be5c
Andrew van Breda <support@avb-it.co.uk>
2021-07-14 14:05:26 +0100

    Make sure the system only ever shows the initially agreed upon sets or terms for English and Welsh, not ones added for other countries or the app

--- Pollinator Monitoring Scheme (PoMS)/Node JS Files/node.2.js
+++ Pollinator Monitoring Scheme (PoMS)/Node JS Files/node.2.js
@@ -1,3 +1,111 @@
+jQuery(document).ready(function($) {
+  // Make sure the system only ever shows the initially agreed upon sets or terms, nothing added for other countries
+  // or the app
+  var habitatNames = ['','Garden','School grounds','Parkland with trees','Churchyard',
+      'Grassy verge or hedgerow edge','Grassland with wild flowers (e.g. meadow)',
+      'Amenity grassland (usually mown short)','Farm crops or grassy pasture',
+      'Upland moorland','Lowland heath',"Brownfield or other 'waste ground'",
+      'Woodland','Other - please describe below',
+      'Gardd','Tir ysgol','Tir parc gyda choed','Mynwent eglwys','Ymyl glaswelltog neu ymyl gwrych',
+      'Glaswelltir gyda blodau gwyllt (e.e. dôl)','Glaswelltir amwynder (wedi ei dorri’n fyr fel arfer)',
+      'Cnydau fferm neu boraf laswelltog','Rhostir ucheldir','Gweundir iseldir','Tir llwyd neu ‘dir gwastraff’ arall',
+      'Coetir','Arall - disgrifiwch isod'];
+	
+  var targetNames = ['','Bramble (Blackberry) - Rubus fruticosus','Buddleja','Buttercup - Ranunculus species',
+      'Dandelion - Taraxacum officinale','Hawthorn - Crataegus','Heathers - Calluna and Erica species',
+      'Hogweed - Heracleum sphondylium','Ivy - Hedera','Knapweeds (Common or Greater) - Centaurea nigra or scabiosa',
+      'Lavender (English) - Lavandula angustifolia','Ragwort - Jacobaea/Senecio species','Thistle - Cirsium or Carduus',
+      'White Clover - Trifolium repens','White Dead-nettle - Lamium album','Other - please describe below',
+      'Miaren/Mwyaren Ddu - Rubus fruticosus','Buddleja','Blodyn menyn - rhywogaethau Ranunculus',
+      'Dant y llew - Taraxacum officinale','Draenen Wen - Crataegus','Grug - Rhywogaethau Calluna ac Erica',
+      'Efwr - Heracleum sphondylium','Iorwg - Hedera','Y Bengaled/Bengaled Fawr - Centaurea nigra neu scabiosa',
+      'Lafant (Saesnig) - Lavandula angustifolia','Llysiau’r Gingroen - rhywogaethau Jacobaea/Senecio',
+      'Ysgall - Carduus neu Cirsium','Meillionen Wen - Trifolium repens',
+      'Marddanhadlen Wen - Lamium album','Arall - disgrifiwch isod'];
+      
+  var typeNames = ['','individual flower','flower head','flower umbel','flower spike',
+      'blodyn unigol','pen blodau','wmbel blodau','pigyn blodau'];
+  
+  var patchOccupationNames = ['','Growing in a larger patch of the same flower','Growing in a larger patch of many different flowers',
+      'More or less isolated','Not recorded',
+      "Mae'r blodau targed yn gorchuddio llai na hanner y llain","Mae'r blodau targed yn gorchuddio tua hanner y llain",
+      "Mae'r blodau targed yn gorchuddio dros hanner y llain","Heb ei gofnodi"];
+      
+  var patchContextNames = ['','Growing in a larger patch of the same flower','Growing in a larger patch of many different flowers',
+      'More or less isolated','Not recorded',
+      'Yn tyfu mewn llain fwy o’r un blodyn','Yn tyfu mewn llain fwy o lawer o flodau gwahanol',
+      'Wedi ei hynysu fwy neu lai','Heb ei gofnodi'];
+      
+  var skyNames = ['','All or mostly blue','Half blue and half cloud','All or mostly cloud','Not recorded',
+      "I gyd neu’r rhan fwyaf yn las","Hanner yn las hanner yn gymylog",
+      "Yn gwbl neu’n bennaf gymylog","Heb ei gofnodi"];
+    
+  var sunShadeNames = ['','Entirely in sunshine','Partly in sun and partly shaded','Entirely shaded','Not recorded',
+      'Yn gyfan gwbl yn yr haul','Rhannol yn yr haul a rhannol yn y cysgod',
+      'Yn gyfan gwbl yn y cysgod','Heb ei gofnodi'];
+  
+  var windNames = ['','Leaves still/moving occasionally','Leaves moving gently all the time',
+      'Leaves moving strongly','Not recorded',
+      'Dail yn llonydd/symud weithiau',"Dail yn symud yn ysgafn drwy’r amser",
+      "Dail yn symud yn sylweddol","Heb ei gofnodi"];
+	
+  // Habitat drop-down
+  jQuery('#smpAttr\\:1048 option').each(function() {
+    if (jQuery.inArray(jQuery(this).text(),habitatNames) === -1) {
+	  jQuery(this).remove();
+    }
+  });
+  
+  // Target flower drop-down
+  jQuery('#smpAttr\\:1050 option').each(function() {
+    if (jQuery.inArray(jQuery(this).text(),targetNames) === -1) {
+	  jQuery(this).remove();
+    }
+  });
+  
+  // Type flower drop-down
+  jQuery('#smpAttr\\:1054 option').each(function() {
+    if (jQuery.inArray(jQuery(this).text(),typeNames) === -1) {
+	  jQuery(this).remove();
+    }
+  });
+  
+  // Patch occupation radio buttons
+  jQuery('#smpAttr\\:1052').find('input').each(function() {
+    if (jQuery.inArray(jQuery(this).text(),patchOccupationNames) === -1) {
+	  jQuery(this).parent('li').remove();
+    }
+  });
+  
+  // Patch context radio buttons
+  jQuery('#smpAttr\\:1055').find('input').each(function() {
+    if (jQuery.inArray(jQuery(this).text(),patchContextNames) === -1) {
+	  jQuery(this).parent('li').remove();
+    }
+  });
+  
+  // Sky above your location
+  jQuery('#smpAttr\\:1057 option').each(function() {
+    if (jQuery.inArray(jQuery(this).text(),skyNames) === -1) {
+	  jQuery(this).remove();
+    }
+  });
+  
+  // Sun or shade
+  jQuery('#smpAttr\\:1061 option').each(function() {
+    if (jQuery.inArray(jQuery(this).text(),sunShadeNames) === -1) {
+	  jQuery(this).remove();
+    }
+  });
+  
+  // Wind strength
+  jQuery('#smpAttr\\:1058 option').each(function() {
+    if (jQuery.inArray(jQuery(this).text(),windNames) === -1) {
+	  jQuery(this).remove();
+    }
+  });
+});
+
 (function($){
   // Closure.
   $(function(){
