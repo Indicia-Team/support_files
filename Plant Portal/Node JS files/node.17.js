d32400ef8c2b4e6b51fad573cd888ab60fd45400
Andrew van Breda <support@avb-it.co.uk>
2022-01-10 16:02:17 +0000

    Cleanup links page after joining a group

--- Plant Portal/Node JS files/node.17.js
+++ Plant Portal/Node JS files/node.17.js
@@ -1,12 +1,21 @@
-// Remove the individual data entry forms from the list of accessible pages when the system
-// confirms to the user that they have joined a group (leaving just the Data Entry selection page visible)
+
 jQuery(document).ready(function () {
-    jQuery('[href*="wildflower-data-entry"]').remove(); 
-    jQuery('[href*="indicator-data-entry"]').remove(); 
-    jQuery('[href*="inventory-data-entry"]').remove();  
-    
     // Remove the parent list items to avoid empty bullet point lines being left on the screen
     jQuery('[href*="wildflower-data-entry"]').closest("li").remove(); 
     jQuery('[href*="indicator-data-entry"]').closest("li").remove(); 
     jQuery('[href*="inventory-data-entry"]').closest("li").remove();  
+
+    jQuery('[href*="npms-mode-data-entry-selection"]').closest('ul').siblings('p').before('<br>');
+
+    jQuery('[href*="npms-mode-data-entry-selection"]').closest('li').before('<hr>');
+    jQuery('<div>Enter your Wildflower, Inventory and Indicator data here</div>').insertBefore('[href*="npms-mode-data-entry-selection"]');
+    
+    jQuery('[href*="list-squares-npms-mode"]').closest('li').before('<hr>');
+    jQuery('<div>View a list of squares for the project here</div>').insertBefore('[href*="list-squares-npms-mode"]');
+
+    jQuery('[href*="my-samples-npms-mode"]').closest('li').before('<hr>');
+    jQuery('<div>View you visits here</div>').insertBefore('[href*="my-samples-npms-mode"]');
+
+    jQuery('[href*="my-projects"]').closest('li').before('<hr>');
+    jQuery('[href*="my-projects"]').text('Return to your projects list');
 });
