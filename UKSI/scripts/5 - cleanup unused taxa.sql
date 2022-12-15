2434a8a77fdfa2a875722d41ac8176f3246e01ad
John van Breda <john.vanbreda@biodiverseit.co.uk>
2022-12-15 11:33:27 +0000

    Taxon paths cache also needs cleanup to prevent fk errors

--- UKSI/scripts/5 - cleanup unused taxa.sql
+++ UKSI/scripts/5 - cleanup unused taxa.sql
@@ -37,6 +37,8 @@ FROM broke_meanings_by_id bm
 WHERE bm.id=u.id
 AND u.taxon_meaning_id<>bm.new_taxon_meaning_id;
 
+DELETE FROM cache_taxon_paths WHERE taxon_meaning_id IN (SELECT old_taxon_meaning_id FROM broke_meanings_by_id);
+
 -- Now get meanings which definitely don't exist any more, so we can fix FKs in
 -- tables which don't have the benefit of being able to use taxa_taxon_list_id
 -- to find the correct taxon_meaning_id.
