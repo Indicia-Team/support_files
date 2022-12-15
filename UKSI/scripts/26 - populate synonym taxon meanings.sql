7d99df025aae92aff661fa805796e8a9a2d79ea4
John van Breda <john.vanbreda@biodiverseit.co.uk>
2022-12-15 11:36:54 +0000

    Fix changed flag bug
    Mustn't overwrite it's previous state or some changes don't get applied.
    Also another organism key matching fix.

--- UKSI/scripts/26 - populate synonym taxon meanings.sql
+++ UKSI/scripts/26 - populate synonym taxon meanings.sql
@@ -6,8 +6,9 @@ DROP TABLE IF EXISTS new_taxon_meanings;
 -- to detect which are actual changes.
 UPDATE uksi.prepared_taxa_taxon_lists pttl
 SET taxon_meaning_id=pttlpref.taxon_meaning_id,
-  changed=pttlpref.taxon_meaning_id<>pttl.orig_taxon_meaning_id
+  changed=pttl.changed OR pttlpref.taxon_meaning_id<>pttl.orig_taxon_meaning_id
 FROM uksi.prepared_taxa_taxon_lists pttlpref
 WHERE pttlpref.recommended_taxon_version_key=pttl.recommended_taxon_version_key
+AND pttlpref.organism_key=pttl.organism_key
 AND pttlpref.preferred=true
 AND pttl.preferred=false;
