5f20d30c66f3f104845412ff28fa5da7dc87ee9c
Andrew van Breda <support@avb-it.co.uk>
2023-01-26 18:24:59 +0000

    Default date to current year

--- Pollinator Monitoring Scheme (PoMS)/Node JS Files/node.4.js
+++ Pollinator Monitoring Scheme (PoMS)/Node JS Files/node.4.js
@@ -1,4 +1,20 @@
 jQuery(document).ready(function($) {
   $('#dynamic-survey_id option[value="638"]').hide();
   $('#dynamic-survey_id option[value="641"]').hide();
-});    
+});
+
+// Always default filter to current year
+jQuery(window).on('load', function() {
+  // Only set default if user hasn't already selected the year
+  if (!jQuery('#dynamic-year_sort_order').val()) {
+    var theYear = new Date().getFullYear();
+    // Need to use minus because of the way the lookup ordering works
+    theYear = '-' + theYear;
+    jQuery('#dynamic-year_sort_order option[value="' + theYear + '"]').prop('selected', true);
+    // Check for the presence of the grid otherwise the page 
+    // gets into a loop reloading and firing clicks
+    if (!jQuery('.report-grid-container').length) {
+      jQuery('#run-report').trigger('click');
+    }
+  }
+});
