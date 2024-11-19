44b57c09c154cfa607b40a94e2dc54d489553285
Andrew van Breda <support@avb-it.co.uk>
2024-11-19 10:41:56 +0000

    Make sure Tracking the Impact layer names are right

--- Plant Portal/Node JS files/node.10.js
+++ Plant Portal/Node JS files/node.10.js
@@ -1,9 +1,3 @@
-function display_grid_ref_info(features) { 
-  if (features[0] && features[0].attributes.id && features[0].attributes.name) {
-      alert("You have clicked on square " + features[0].attributes.name + ".");
-  }
-}
-
 mapInitialisationHooks.push(function (div) {
   jQuery.each(div.map.layers, function(idx, layer) {
     if (layer.name === "Ordnance Survey Outdoor") {
