7e013d99eba40c7f81a876ea99199a307ceed117
Andrew van Breda <support@avb-it.co.uk>
2024-11-19 10:57:41 +0000

    Make sure layer names are correct on Plant Portal standard mode.

--- Plant Portal/Node JS files/node.37.js
+++ Plant Portal/Node JS files/node.37.js
@@ -193,3 +193,14 @@ function set_column_to_show_hide(attrId, inputClass, action) {
     jQuery('.'+ inputClass).val('');
   }
 }
+
+mapInitialisationHooks.push(function (div) {
+  jQuery.each(div.map.layers, function(idx, layer) {
+    if (layer.name === "Ordnance Survey Outdoor") {
+      layer.name = "OS Outdoor";
+    }
+    if (layer.name === "Dynamic (*OpenStreetMap* > Ordnance Survey Leisure > Google Satellite)") {
+      layer.name = "Dynamic";
+    }
+  });
+});
