39e19820ec9d00f8beaf6bd3373ffe64a5b7a7d4
Andrew van Breda <support@avb-it.co.uk>
2025-03-15 14:06:00 +0000

    ABLE issue 462, cleanup of Add Site Section Details tab.

---
+++ EBMS & UKBMS/EBMS/Node files/JS/node.31.js
@@ -0,0 +1,5 @@
+jQuery(document).ready(function($) {
+  $('#ctrl-wrap-locAttr-220').insertAfter('#section-location-system');
+  $('#fieldset-section').remove();
+  $("legend:contains('Habitat etc')").remove();
+});
