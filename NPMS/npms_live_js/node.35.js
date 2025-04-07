0ce8f1068e8d72eb2054b39e4f799b3f4cfeadf8
Andrew van Breda <support@avb-it.co.uk>
2025-04-07 10:14:08 +0100

    Further code from Julian which hides the event booking button when logged out

--- NPMS/npms_live_js/node.35.js
+++ NPMS/npms_live_js/node.35.js
@@ -1,9 +1,22 @@
-var trainingEventsImageReplace;
+var trainingEventsGridFurtherSetup;
+
 jQuery(document).ready(function () {
+  trainingEventsGridFurtherSetup = function() {
+    trainingEventsImageReplace();
+    trainingEventsLoginButtons();
+  }
+
   // Get full size images instead of thumbnails on grid
-  trainingEventsImageReplace = function trainingEventsImageReplace() {
+   function trainingEventsImageReplace() {
     jQuery('.col-images img').each(function(){
       jQuery(this).attr('src', jQuery(this).attr('src').replace('thumb-', ''))
     });
+  }
+
+  function trainingEventsLoginButtons() {
+    if ((!(jQuery('body').hasClass('user-logged-in'))) && (!(jQuery('body').hasClass('event-login-btns')))) {
+        jQuery('body').addClass('event-login-btns');
+        jQuery('.report-grid-container > table > tbody > tr').append('<td class="col-actions"><a class="disabled action-button add-my-event-link">Register/Login above to book</a></td>');
+    }
   }
 });
