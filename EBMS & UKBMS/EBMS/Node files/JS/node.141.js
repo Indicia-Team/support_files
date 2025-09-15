03c8bdac173c36e7aeb8602e959956543ac13309
Andrew van Breda <support@avb-it.co.uk>
2025-09-15 17:05:15 +0100

    Code to support Projects drop-down for EBMS. Hides drop-down if no projects selected. Makes projects read-only if already selected.

---
+++ EBMS & UKBMS/EBMS/Node files/JS/node.141.js
@@ -0,0 +1,11 @@
+// Check if less than 2 items before hiding as there is always a Please Select item
+// that we shouldn't count.
+jQuery(document).ready(function($) {
+  if($('#sample\\:group_id > option').length < 2) {
+    $('#ctrl-wrap-sample-group_id').hide();
+  }
+  // Do not allow changing of existing group
+  if ($('#sample\\:group_id').val()) {
+    $('#sample\\:group_id').prop("disabled", true);
+  }
+});
