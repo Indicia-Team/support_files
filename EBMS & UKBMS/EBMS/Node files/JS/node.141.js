a2f870f4ad65c6af727a461202a9fe0be55e563e
Andrew van Breda <support@avb-it.co.uk>
2026-01-22 12:12:19 +0000

    Don't prevent switching of project if sample is already assigned to a project (ABLE issue 835).

--- EBMS & UKBMS/EBMS/Node files/JS/node.141.js
+++ EBMS & UKBMS/EBMS/Node files/JS/node.141.js
@@ -4,10 +4,6 @@ jQuery(document).ready(function($) {
   if($('#sample\\:group_id > option').length < 2) {
     $('#ctrl-wrap-sample-group_id').hide();
   }
-  // Do not allow changing of existing group
-  if ($('#sample\\:group_id').val()) {
-    $('#sample\\:group_id').prop("disabled", true);
-  }
   // Change "Select group" label to "Select project"
   $("[for='sample\\:group_id']").html('Select project:');
 });
