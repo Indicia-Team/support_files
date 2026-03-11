6670a3fbb8eb6141f77bc15dbdb4c685a228bf7d
Andrew van Breda <support@avb-it.co.uk>
2026-03-11 16:17:04 +0000

    Show and hide day active moths option depending on where Moths species list option is showing.

--- EBMS & UKBMS/EBMS/Node files/JS/node.195.js
+++ EBMS & UKBMS/EBMS/Node files/JS/node.195.js
@@ -3,7 +3,20 @@
     $('#dynamic-taxon_list_filter option[value=""]').text("<all>");
     $('#dynamic-country_region_ttl_attr_id option[value=""]').text("<all>");
     $('#dynamic-species_presence_filter option[value=""]').text("<all>");
-    // Default species presence to "P"
-    $('#dynamic-common_names_language option[value=""]').text("<none>");
+    // Default species presence to "P" (comment this out for now ,as many species don't have presence set)
+    //$('#dynamic-common_names_language option[value=""]').text("<none>");
+    show_hide_day_active_moths_checkbox();
   });
+  $('#dynamic-taxon_list_filter').change(function() {
+    show_hide_day_active_moths_checkbox();
+  });
+  
+  function show_hide_day_active_moths_checkbox() {
+    if ($('#dynamic-taxon_list_filter').val() === '260') {
+      $('#ctrl-wrap-dynamic-day_active_moths').show();
+    } else {
+      $('#ctrl-wrap-dynamic-day_active_moths').hide();
+      $('#dynamic-day_active_moths').val('');
+    }
+  }
 }) (jQuery);
