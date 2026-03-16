68fedce09bee48eb928e637e6e3db753fadf915a
Andrew van Breda <support@avb-it.co.uk>
2026-03-16 17:50:46 +0000

    Day active moths empty option should show "<all>"

--- EBMS & UKBMS/EBMS/Node files/JS/node.195.js
+++ EBMS & UKBMS/EBMS/Node files/JS/node.195.js
@@ -3,6 +3,8 @@
     $('#dynamic-taxon_list_filter option[value=""]').text("<all>");
     $('#dynamic-country_region_ttl_attr_id option[value=""]').text("<all>");
     $('#dynamic-species_presence_filter option[value=""]').text("<all>");
+    $('#dynamic-day_active_moths option[value=""]').text("<all>");
+
     // Default species presence to "P" (comment this out for now ,as many species don't have presence set)
     //$('#dynamic-common_names_language option[value=""]').text("<none>");
     show_hide_day_active_moths_checkbox();
