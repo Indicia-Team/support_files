57f2180d0a1d71dd0b969ed12d31c8d1b233cd13
Andrew van Breda <support@avb-it.co.uk>
2026-08-12 11:42:41 +0100

    Page now supports multiple surveys, not just transects.

--- EBMS & UKBMS/EBMS/Node files/JS/node.100.js
+++ EBMS & UKBMS/EBMS/Node files/JS/node.100.js
@@ -10,22 +10,37 @@ jQuery(document).ready(function($) {
       var row = $('<tr><td>' + this.key + '</td><td class="subtable"></td></tr>')
         .appendTo(tbody);
       var subTable = $('<table class="table" />');
-      var subThead = $('<thead><tr><th>Year</th><th>Sites</th><th>Samples</th><th>Occurrences</th><th>Species</th></tr></thead>')
+      var subThead = $('<thead><tr><th class="col-year">Year</th><th class="col-sites">Sites</th><th class="col-child-sites">Sites</th><th class="col-smps">Samples</th><th class="col-child-smps">Samples</th><th class="col-occs">Occurrences</th><th class="col-species">Species</th></tr></thead>')
         .appendTo(subTable);
       var subTbody = $('<tbody />')
         .appendTo(subTable);
       var subBuckets = this.back_to_occurrence.by_year.buckets;
       $.each(subBuckets, function eachSubbucket() {
-        $('<tr><td>' + this.key + '</td>' +
-          '<td>' + this.transects.value + '</td>' +
-          '<td>' + this.samples.value + '</td>' +
-          '<td>' + this.doc_count + '</td>' +
-          '<td>' + this.species.value + '</td></tr>')
+        $('<tr><td class="col-year">' + this.key + '</td>' +
+          '<td class="col-sites">' + this.sites.value + '</td>' +
+          '<td class="col-child-sites">' + this.child_sites.value + '</td>' +
+          '<td class="col-smps">' + this.samples.value + '</td>' +
+          '<td class="col-child-smps">' + this.child_samples.value + '</td>' +
+          '<td class="col-occs">' + this.doc_count + '</td>' +
+          '<td class="col-species">' + this.species.value + '</td></tr>')
           .appendTo(subTbody);
       })
       subTable.appendTo(row.find('.subtable'));
     });
     $(el).html('');
     $(el).append(table);
+    // If Moth Trap survey, we don't want to show parent sites/sample
+    // as the survey structure is different.
+    if ($('#filter-survey').val() == 681) {
+      $('.col-child-sites').show();
+      $('.col-child-smps').show();
+      $('.col-sites').hide();
+      $('.col-smps').hide();
+    } else {
+      $('.col-child-sites').hide();
+      $('.col-child-smps').hide();
+      $('.col-sites').show();
+      $('.col-smps').show();
+    }
   }
 });
